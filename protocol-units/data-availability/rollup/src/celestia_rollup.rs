49ef50fa3e430cc8c48b6699dcbd82d0492a2d2b
Richard Melkonian <r.v.melkonian@gmail.com>
2024-03-04 15:00:58 +0000

    create mock rollup

--- protocol-units/data-availability/rollup/src/celestia_rollup.rs
+++ protocol-units/data-availability/rollup/src/celestia_rollup.rs
@@ -19,7 +19,7 @@ use sov_stf_runner::RollupConfig;
 use sov_stf_runner::RollupProverConfig;
 use stf::Runtime;
 
-/// The namespace for the rollup on Celestia. 
+/// The namespace for the rollup on Celestia.
 const ROLLUP_NAMESPACE: Namespace = Namespace::const_v0(b"sov-celest");
 
 /// The rollup stores the zk proofs in the namespace b"sov-test-p" on Celestia.
@@ -30,7 +30,35 @@ pub struct CelestiaRollup {}
 
 /// This is the place, where all the rollup components come together and
 /// they can be easily swapped with alternative implementations as needed.
-// #[async_trait]
-// impl RollupBlueprint for CelestiaRollup {
+#[async_trait]
+impl RollupBlueprint for CelestiaRollup {
+    type DaSerive = CelesitaSerive;
+    type DaSpec = CelestiaSpec;
+    type DaConfig = CelestiaConfig;
+    type Vm = Risc0Host<'static>;
 
-// }
+    type ZkContext = ZkDefaultContext;
+    type NativeContext = DefaultContext;
+
+    type StorageManager = ProverStoragerManager<CelestiaSpec, DefaultStorageSpec>;
+    type ZkRuntime = Runtime<Self::ZkContext, Self::DaSpec>;
+
+    type NativeRuntime = Runtime<Self::NativeContext, Self::DaSpec>;
+
+    type NativeKernel = BasicKernel<Self::NativeContext, Self::DaSpec>;
+    type ZkKernel = BasicKernel<Self::ZkContext, Self::DaSpec>;
+
+    type ProverService = ParallelProverService<
+        <<Self::NativeContext as Spec>::Storage as Storage>::Root,
+        <<Self::NativeContext as Spec>::Storage as Storage>::Witness,
+        Self::DaService,
+        Self::Vm,
+        StfBlueprint<
+            Self::ZkContext,
+            Self::DaSpec,
+            <Self::Vm as ZkvmHost>::Guest,
+            Self::ZkRuntime,
+            Self::ZkKernel,
+        >,
+    >;
+}
