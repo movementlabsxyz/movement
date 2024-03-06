1c9af882a5fac20c43f83e3ce69628ff475e0b02
Richard Melkonian <r.v.melkonian@gmail.com>
2024-03-06 20:16:42 +0000

    add tests

--- m2/contracts/eth-settlement/test/Settlement.t.sol
+++ m2/contracts/eth-settlement/test/Settlement.t.sol
@@ -1,16 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {Test} from "forge-std/Test.sol";
+import {console2} from "forge-std/console2.sol";
+
 import "ds-test/test.sol";
 import "../src/Settlement.sol";
 import "forge-std/Vm.sol";
+import {
+    IRiscZeroVerifier,
+    Output,
+    OutputLib,
+    Receipt as RiscZeroReceipt,
+    ReceiptClaim,
+    ReceiptClaimLib,
+    ExitCode,
+    SystemExitCode
+} from "../src/IRiscZeroVerifier.sol";
+import {TestReceipt} from "./TestReceipt.sol";
 
-contract SettlementTest is DSTest {
+contract SettlementTest is Test {
+    using OutputLib for Output;
+    using ReceiptClaimLib for ReceiptClaim;
+
     Vm vm = Vm(HEVM_ADDRESS);
     Settlement settlement;
     address signer1 = address(0x1);
     address signer2 = address(0x2);
     bytes exampleProofData = "exampleProof";
+
+    RiscZeroReceipt internal TEST_RECEIPT = RiscZeroReceipt(
+        TestReceipt.SEAL,
+        ReceiptClaim(
+            TestReceipt.IMAGE_ID,
+            TestReceipt.POST_DIGEST,
+            ExitCode(SystemExitCode.Halted, 0),
+            bytes32(0x0000000000000000000000000000000000000000000000000000000000000000),
+            Output(sha256(TestReceipt.JOURNAL), bytes32(0)).digest()
+        )
+    );
 
     function setUp() public {
         settlement = new Settlement();
@@ -26,10 +54,10 @@ contract SettlementTest is DSTest {
         assertTrue(!settlement.isSigner(signer1), "signer1 should not be a signer after removal");
     }
 
-    function testFailSettleNotSigner() public {
-        vm.prank(signer2);
-        settlement.settle(1, exampleProofData);
-    }
+    // function testFailSettleNotSigner() public {
+    //     vm.prank(signer2);
+    //     settlement.settle(1, exampleProofData);
+    // }
 
     function testSettleAndRetrieve() public {
         vm.prank(signer1);
