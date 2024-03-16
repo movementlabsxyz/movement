bb5ed7d0a6240554a95e71d672460562c19fc79e
Richard Melkonian <r.v.melkonian@gmail.com>
2024-03-16 14:23:58 +0000

    add tests

---
+++ m2/rollup/tests/test_helpers.rs
@@ -0,0 +1,68 @@
+use std::net::SocketAddr;
+
+use sov_mock_da::{MockAddress, MockDaConfig};
+use sov_modules_rollup_blueprint::RollupBlueprint;
+use sov_modules_stf_blueprint::kernels::basic::BasicKernelGenesisConfig;
+use sov_modules_stf_blueprint::kernels::basic::BasicKernelGenesisPaths;
+use sov_rollup_starter::mock_rollup::MockRollup;
+use sov_stf_runner::ProverServiceConfig;
+use sov_stf_runner::RollupProverConfig;
+use sov_stf_runner::{RollupConfig, RpcConfig, RunnerConfig, StorageConfig};
+use stf_starter::genesis_config::GenesisPaths;
+use tokio::sync::oneshot;
+
+pub async fn start_rollup(
+    rpc_reporting_channel: oneshot::Sender<SocketAddr>,
+    rt_genesis_paths: GenesisPaths,
+    kernel_genesis_paths: BasicKernelGenesisPaths,
+    rollup_prover_config: RollupProverConfig,
+) {
+    let temp_dir = tempfile::tempdir().unwrap();
+    let temp_path = temp_dir.path();
+
+    let rollup_config = RollupConfig {
+        storage: StorageConfig {
+            path: temp_path.to_path_buf(),
+        },
+        runner: RunnerConfig {
+            start_height: 1,
+            rpc_config: RpcConfig {
+                bind_host: "127.0.0.1".into(),
+                bind_port: 0,
+            },
+        },
+        da: MockDaConfig {
+            sender_address: MockAddress::from([0; 32]),
+        },
+        prover_service: ProverServiceConfig {
+            aggregated_proof_block_jump: 1,
+        },
+    };
+
+    let mock_rollup = MockRollup {};
+
+    let kernel_genesis = BasicKernelGenesisConfig {
+        chain_state: serde_json::from_str(
+            &std::fs::read_to_string(&kernel_genesis_paths.chain_state)
+                .expect("Failed to read chain_state genesis config"),
+        )
+        .expect("Failed to parse chain_state genesis config"),
+    };
+
+    let rollup = mock_rollup
+        .create_new_rollup(
+            &rt_genesis_paths,
+            kernel_genesis,
+            rollup_config,
+            rollup_prover_config,
+        )
+        .await
+        .unwrap();
+    rollup
+        .run_and_report_rpc_port(Some(rpc_reporting_channel))
+        .await
+        .unwrap();
+
+    // Close the tempdir explicitly to ensure that rustc doesn't see that it's unused and drop it unexpectedly
+    temp_dir.close().unwrap();
+}
--- m2/stf/src/genesis_config.rs
+++ m2/stf/src/genesis_config.rs
@@ -1,97 +1,86 @@
-//! This module implements the various "hooks" that are called by the STF during execution.
-//! These hooks can be used to add custom logic at various points in the slot lifecycle:
-//! - Before and after each transaction is executed.
-//! - At the beginning and end of each batch ("blob")
-//! - At the beginning and end of each slot (DA layer block)
+use std::path::{Path, PathBuf};
 
-use super::runtime::Runtime;
-use sov_accounts::AccountsTxHook;
-use sov_bank::BankTxHook;
-use sov_modules_api::hooks::{ApplyBlobHooks, FinalizeHook, SlotHooks, TxHooks};
-use sov_modules_api::transaction::Transaction;
-use sov_modules_api::{AccessoryWorkingSet, BlobReaderTrait, Context, DaSpec, Spec, WorkingSet};
-use sov_modules_stf_blueprint::{RuntimeTxHook, SequencerOutcome};
-use sov_sequencer_registry::SequencerRegistry;
-use sov_state::Storage;
-use tracing::info;
+use anyhow::{bail, Context as _};
+use sov_accounts::AccountConfig;
+use sov_bank::BankConfig;
+use sov_modules_api::{Context, DaSpec};
+use sov_modules_stf_blueprint::Runtime as RuntimeTrait;
+use sov_sequencer_registry::SequencerConfig;
+use sov_stf_runner::read_json_file;
 
-impl<C: Context, Da: DaSpec> TxHooks for Runtime<C, Da> {
-    type Context = C;
-    type PreArg = RuntimeTxHook<C>;
-    type PreResult = C;
+use super::GenesisConfig;
+use crate::Runtime;
 
-    fn pre_dispatch_tx_hook(
-        &self,
-        tx: &Transaction<Self::Context>,
-        working_set: &mut WorkingSet<C>,
-        arg: &RuntimeTxHook<C>,
-    ) -> anyhow::Result<C> {
-        let RuntimeTxHook { height, sequencer } = arg;
-        let AccountsTxHook { sender, sequencer } =
-            self.accounts
-                .pre_dispatch_tx_hook(tx, working_set, sequencer)?;
+/// Paths to genesis files.
+pub struct GenesisPaths {
+    /// Accounts genesis path.
+    pub accounts_genesis_path: PathBuf,
+    /// Bank genesis path.
+    pub bank_genesis_path: PathBuf,
+    /// Sequencer Registry genesis path.
+    pub sequencer_genesis_path: PathBuf,
+}
 
-        let hook = BankTxHook { sender, sequencer };
-        self.bank.pre_dispatch_tx_hook(tx, working_set, &hook)?;
-
-        Ok(C::new(hook.sender, hook.sequencer, *height))
-    }
-
-    fn post_dispatch_tx_hook(
-        &self,
-        tx: &Transaction<Self::Context>,
-        ctx: &C,
-        working_set: &mut WorkingSet<C>,
-    ) -> anyhow::Result<()> {
-        self.accounts.post_dispatch_tx_hook(tx, ctx, working_set)?;
-        self.bank.post_dispatch_tx_hook(tx, ctx, working_set)?;
-        Ok(())
+impl GenesisPaths {
+    /// Creates a new [`GenesisPaths`] from the files contained in the given
+    /// directory.
+    ///
+    /// Take a look at the contents of the `test_data` directory to see the
+    /// expected files.
+    pub fn from_dir(dir: impl AsRef<Path>) -> Self {
+        Self {
+            accounts_genesis_path: dir.as_ref().join("accounts.json"),
+            bank_genesis_path: dir.as_ref().join("bank.json"),
+            sequencer_genesis_path: dir.as_ref().join("sequencer_registry.json"),
+        }
     }
 }
 
-impl<C: Context, Da: DaSpec> ApplyBlobHooks<Da::BlobTransaction> for Runtime<C, Da> {
-    type Context = C;
-    type BlobResult =
-        SequencerOutcome<<<Da as DaSpec>::BlobTransaction as BlobReaderTrait>::Address>;
+/// Creates genesis configuration.
+pub(crate) fn get_genesis_config<C: Context, Da: DaSpec>(
+    genesis_paths: &GenesisPaths,
+) -> Result<<Runtime<C, Da> as RuntimeTrait<C, Da>>::GenesisConfig, anyhow::Error> {
+    let genesis_config =
+        create_genesis_config(genesis_paths).context("Unable to read genesis configuration")?;
 
-    fn begin_blob_hook(
-        &self,
-        blob: &mut Da::BlobTransaction,
-        working_set: &mut WorkingSet<C>,
-    ) -> anyhow::Result<()> {
-        // Before executing each batch, check that the sender is regsitered as a sequencer
-        self.sequencer_registry.begin_blob_hook(blob, working_set)
+    validate_config(genesis_config)
+}
+
+fn validate_config<C: Context, Da: DaSpec>(
+    genesis_config: <Runtime<C, Da> as RuntimeTrait<C, Da>>::GenesisConfig,
+) -> Result<<Runtime<C, Da> as RuntimeTrait<C, Da>>::GenesisConfig, anyhow::Error> {
+    let token_address = &sov_bank::get_genesis_token_address::<C>(
+        &genesis_config.bank.tokens[0].token_name,
+        genesis_config.bank.tokens[0].salt,
+    );
+
+    let coins_token_addr = &genesis_config
+        .sequencer_registry
+        .coins_to_lock
+        .token_address;
+
+    if coins_token_addr != token_address {
+        bail!(
+            "Wrong token address in `sequencer_registry_config` expected {} but found {}",
+            token_address,
+            coins_token_addr
+        )
     }
 
-    fn end_blob_hook(
-        &self,
-        result: Self::BlobResult,
-        working_set: &mut WorkingSet<C>,
-    ) -> anyhow::Result<()> {
-        // After processing each blob, reward or slash the sequencer if appropriate
-        match result {
-            SequencerOutcome::Rewarded(_reward) => {
-                // TODO: Process reward here or above.
-                <SequencerRegistry<C, Da> as ApplyBlobHooks<Da::BlobTransaction>>::end_blob_hook(
-                    &self.sequencer_registry,
-                    sov_sequencer_registry::SequencerOutcome::Completed,
-                    working_set,
-                )
-            }
-            SequencerOutcome::Ignored => Ok(()),
-            SequencerOutcome::Slashed {
-                reason,
-                sequencer_da_address,
-            } => {
-                info!("Sequencer {} slashed: {:?}", sequencer_da_address, reason);
-                <SequencerRegistry<C, Da> as ApplyBlobHooks<Da::BlobTransaction>>::end_blob_hook(
-                    &self.sequencer_registry,
-                    sov_sequencer_registry::SequencerOutcome::Slashed {
-                        sequencer: sequencer_da_address,
-                    },
-                    working_set,
-                )
-            }
-        }
-    }
+    Ok(genesis_config)
+}
+
+fn create_genesis_config<C: Context, Da: DaSpec>(
+    genesis_paths: &GenesisPaths,
+) -> anyhow::Result<GenesisConfig<C, Da>> {
+    let accounts_config: AccountConfig<C> = read_json_file(&genesis_paths.accounts_genesis_path)?;
+    let bank_config: BankConfig<C> = read_json_file(&genesis_paths.bank_genesis_path)?;
+    let sequencer_registry_config: SequencerConfig<C, Da> =
+        read_json_file(&genesis_paths.sequencer_genesis_path)?;
+
+    Ok(GenesisConfig::new(
+        accounts_config,
+        bank_config,
+        sequencer_registry_config,
+    ))
 }
