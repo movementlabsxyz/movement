fd8dd54c5b87ef81682be57e0c275127189bdc8d
Liam Monninger <l.mak.monninger@gmail.com>
2024-04-18 16:19:32 -0700

    fix: dropping localization script.

--- scripts/localize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Example script that could replace a placeholder in Cargo.toml.template
-PWD=$(pwd)
-sed "s|PWD|file://${PWD}/vendors/sibling-workspace|" Cargo.toml.template > Cargo.toml
-
-# Then run your cargo build or cargo run commands
-cargo build
