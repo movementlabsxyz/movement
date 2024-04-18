cc2b20f962196de1a03aadfa3d9d20a39b61031c
Richard Melkonian <r.v.melkonian@gmail.com>
2024-04-18 11:32:21 +0100

    update modules

--- protocol-units/dispute/test/Counter.t.sol
+++ protocol-units/dispute/test/Counter.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Dispute} from "../src/Dispute.sol";
 
 contract CounterTest is Test {
     Counter public counter;
