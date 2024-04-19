e8331db5c21f1f887fd0b632a661f808759929ee
Liam Monninger <l.mak.monninger@gmail.com>
2024-04-19 14:25:52 -0700

    fix: celestia startup.

--- celestia-app.nix
+++ celestia-app.nix
@@ -1,24 +1,24 @@
 { pkgs,  }:
 
 pkgs.buildGoModule rec {
-  pname = "celestia-node";
-  version = "0.13.3";
+  pname = "celestia-app";
+  version = "v1.8.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "celestiaorg";
-    repo = "celestia-node";
-    rev = "05238b3e087eb9ecd3b9684cd0125f2400f6f0c7";
+    repo = "celestia-app";
+    rev = "e75a1fdc8f2386d9f389cb596c88ca7cc19563af";
     sha256 = "sha256-YCwIJ55lkLcViVzmAeCIrPtc9mJ/N0eswKrlu9BEC3g=";  # Replace with the actual sha256
   };
 
   vendorHash = "sha256-UyNNVDO/FFKp80rI5kOI4xfKpkhqF53lgiOSJhCm79U=";  # Replace with the correct vendor hash
 
   # Specify the subpackage to build
-  subPackages = [ "cmd/celestia" "cmd/cel-key" ];
+  subPackages = [ "cmd/celestia-appd" ];
 
   meta = with pkgs.lib; {
     description = "Build specific Go subpackage in Nix";
-    homepage = "https://github.com/celestiaorg/celestia-node";
+    homepage = "https://github.com/celestiaorg/celestia-app";
     license = licenses.mit;
     maintainers = with maintainers; [ maintainers.example ];
   };
--- justfile
+++ justfile
@@ -1,4 +1,6 @@
 m1-da-light-node-test-native:
+    cargo build -p m1-da-light-node
     scripts/movement/test-native m1-da-light-node
 m1-da-light-node-run-native:
+    cargo build -p m1-da-light-node
     scripts/movement/run-native m1-da-light-node
