e88eadb38da6975dd903c3d4b77e5b56c9ff009d
Liam Monninger <l.mak.monninger@gmail.com>
2024-04-18 17:59:32 -0700

    fix: nix install workaround.

--- celestia-node.nix
+++ celestia-node.nix
@@ -1,8 +1,8 @@
 { pkgs }:
 
 pkgs.stdenv.mkDerivation rec {
-  pname = "celestia-node";
-  version = "0.13.2";
+  name = "celestia-node";
+  version = "v0.13.2"; # Update to the desired version
   commit_hash = "c1b41b0973e9d140b7651295e879d27ad47f42c4";
 
   src = builtins.fetchGit {
@@ -12,7 +12,17 @@ pkgs.stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = with pkgs; [
-    git go gnumake gcc protobuf clang llvm openssl rustc cargo coreutils
+    git
+    go
+    gnumake
+    gcc
+    protobuf
+    clang
+    llvm
+    openssl
+    rustc
+    cargo
+    coreutils
   ];
 
   preBuild = ''
@@ -22,14 +32,21 @@ pkgs.stdenv.mkDerivation rec {
     mkdir -p $GOPATH $GOCACHE
   '';
 
-  buildFlags = [ "build" "cel-key" ];
+  buildPhase = ''
+    export HOME=$TMPDIR
+    export GOPATH="$TMPDIR/go"
+    export GOCACHE="$TMPDIR/go-cache"
+    mkdir -p $GOPATH $GOCACHE
+    make build && make install
+    make cel-key && make install-key
+  '';
 
   installPhase = ''
     mkdir -p $out/bin
-    ls -la $TMPDIR/go
-    cp $GOPATH/bin/celestia $out/bin/celestia
-    cp $GOPATH/bin/cel-key $out/bin/cel-key
+    cp $GOPATH/bin/celestia $out/bin
+    cp $GOPATH/bin/cel-key $out/bin
   '';
+
 
   meta = with pkgs.lib; {
     description = "Celestia Node";
