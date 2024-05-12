4297dd3d25f115f5b569ca575ef0d8a4b314837e
Liam Monninger <l.mak.monninger@gmail.com>
2024-05-12 13:17:24 -0700

    fix: try monza derivation.

--- flake.nix
+++ flake.nix
@@ -33,29 +33,7 @@
         celestia-app = import ./celestia-app.nix { inherit pkgs; };
 
         # monza-aptos
-        monza-aptos = pkgs.stdenv.mkDerivation {
-          pname = "monza-aptos";
-          version = "branch-monza";
-
-          src = pkgs.fetchFromGitHub {
-              owner = "movementlabsxyz";
-              repo = "aptos-core";
-              rev = "06443b81f6b8b8742c4aa47eba9e315b5e6502ff";
-              sha256 = "sha256-iIYGbIh9yPtC6c22+KDi/LgDbxLEMhk4JJMGvweMJ1Q=";
-          };
-
-          installPhase = ''
-              cp -r . $out
-          '';
-
-          meta = with pkgs.lib; {
-              description = "Aptos core repository on the monza branch";
-              homepage = "https://github.com/movementlabsxyz/aptos-core";
-              license = licenses.asl20;
-          };
-
-
-        };
+        monza-aptos = import ./monza-aptos.nix { inherit pkgs; };
        
         # Specific version of toolchain
         rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
