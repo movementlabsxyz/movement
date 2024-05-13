e4a5b6593be9202698e7311874cd6e556b07c10e
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2024-05-13 15:00:02 +0300

    mcr-settlement-client: fmt

--- protocol-units/settlement/mcr/client/src/lib.rs
+++ protocol-units/settlement/mcr/client/src/lib.rs
@@ -7,19 +7,20 @@ pub mod stub;
 #[cfg(feature = "stub")]
 pub use stub::*;
 
-
-type CommitmentStream = std::pin::Pin<Box<dyn Stream<Item = Result<BlockCommitment, anyhow::Error>> + Send>>;
+type CommitmentStream =
+	std::pin::Pin<Box<dyn Stream<Item = Result<BlockCommitment, anyhow::Error>> + Send>>;
 
 #[tonic::async_trait]
 pub trait McrSettlementClientOperations {
+	async fn post_block_commitment(
+		&self,
+		block_commitment: BlockCommitment,
+	) -> Result<(), anyhow::Error>;
 
-    async fn post_block_commitment(&self, block_commitment : BlockCommitment) -> Result<(), anyhow::Error>;
+	async fn stream_block_commitments(&self) -> Result<CommitmentStream, anyhow::Error>;
 
-    async fn stream_block_commitments(&self) -> Result<
-        CommitmentStream,
-        anyhow::Error
-    >;
-
-    async fn get_commitment_at_height(&self, height : u64) -> Result<Option<BlockCommitment>, anyhow::Error>;
-
+	async fn get_commitment_at_height(
+		&self,
+		height: u64,
+	) -> Result<Option<BlockCommitment>, anyhow::Error>;
 }
