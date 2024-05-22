f304e19ca4489918ac3bf6d0f9c1253caafa4d21
Code8 <code8@movementlabs.xyz>
2024-05-22 17:51:53 +0200

    refactor: trait naming and surpressing warnings
    Why:
    - Refactored the trait name `SuzukaFullNode` to `SuzukaNode` for better
      clarity. As the SuzukaPartialNode implements the trait, FullNode seems
      confusing.
    - Added `#[allow(async_fn_in_trait)]` attribute to supress async function warning.
    
    Changes:
    - Renamed trait `SuzukaFullNode` to `SuzukaNode`.
    - Added `#[allow(async_fn_in_trait)]` attribute to the `SuzukaNode` trait in lib.rs file.

--- networks/suzuka/suzuka-full-node/src/lib.rs
+++ networks/suzuka/suzuka-full-node/src/lib.rs
@@ -3,28 +3,22 @@ pub mod partial;
 #[cfg(test)]
 pub mod tests;
 
-pub trait SuzukaFullNode {
+#[allow(async_fn_in_trait)]
+pub trait SuzukaNode {
+	/// Runs the services until crash or shutdown.
+	async fn run_services(&self) -> Result<(), anyhow::Error>;
 
-    /// Runs the services until crash or shutdown.
-    async fn run_services(&self) -> Result<(), anyhow::Error>;
+	/// Runs the background tasks until crash or shutdown.
+	async fn run_background_tasks(&self) -> Result<(), anyhow::Error>;
 
-    /// Runs the background tasks until crash or shutdown.
-    async fn run_background_tasks(&self) -> Result<(), anyhow::Error>;
+	/// Runs the executor until crash or shutdown.
+	async fn run_executor(&self) -> Result<(), anyhow::Error>;
 
-    /// Runs the executor until crash or shutdown.
-    async fn run_executor(&self) -> Result<(), anyhow::Error>;
+	/// Runs the full node until crash or shutdown.
+	async fn run(&self) -> Result<(), anyhow::Error> {
+		// run services and executor concurrently
+		tokio::try_join!(self.run_background_tasks(), self.run_services(), self.run_executor())?;
 
-    /// Runs the full node until crash or shutdown.
-    async fn run(&self) -> Result<(), anyhow::Error> {
-        
-        // run services and executor concurrently
-        tokio::try_join!(
-            self.run_background_tasks(),
-            self.run_services(),
-            self.run_executor()
-        )?;
-
-        Ok(())
-    }
-
+		Ok(())
+	}
 }
