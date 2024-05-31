27db9ebd22f83deec7c7b263716c367993a6b697
radupopa369 <radupopa21be@gmail.com>
2024-05-31 08:49:37 +0300

    try crane source filtering

--- nix/m1-da-light-node.nix
+++ nix/m1-da-light-node.nix
@@ -2,13 +2,6 @@
 
 let
 
-    allFilter = path: _type: true;
-
-    src = pkgs.lib.cleanSourceWith {
-        src = craneLib.path ./..; # The original, unfiltered source
-        filter = allFilter;
-    };
-
     # Common arguments can be set here to avoid repeating them later
     commonArgs = {
         inherit src;
@@ -19,6 +12,7 @@ let
         ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
         pkgs.libiconv
         ];
+      
     };
 
     cargoArtifacts = craneLib.buildDepsOnly commonArgs;
@@ -39,18 +33,37 @@ let
             # I think something should go here, to include the `vendor-cargo-deps`, but I'm not sure what.
         ];
     };
+    
+    # bplFilter = path: _type: builtins.match ".*bpl$" path != null;
+    bplFilter = path: _type: builtins.match ".*" path != null;
+    bplOrCargo = path: type:
+        (bplFilter path type) || (craneLib.filterCargoSources path type);
+
+
+    # src = pkgs.lib.cleanSourceWith {
+    #     src = craneLib.path ./..; # The original, unfiltered source
+    #     filter = bplOrCargo;
+    # };
+
+    # src = craneLib.path ./..; # The original, unfiltered source
+    src = craneLib.cleanCargoSource (craneLib.path ./..);
 
     in
+    # craneLib.buildPackage {
     pkgs.stdenv.mkDerivation {
     pname = "m1-da-light-node";
     version = "0.1.0";
 
-    inherit src;
+    # inherit src;
+
 
     m1-da-light-node = craneLib.buildPackage (individualCrateArgs // {
+        # inherit src ;
         pname = "m1-da-light-node";
         cargoExtraArgs = "-p m1-da-light-node";
-        src = fileSetForCrate ../protocol-units/da/m1/light-node;
+        # src = src ;
+        # src = fileSetForCrate ../protocol-units/da/m1/light-node;
+
     });
 
     meta = with pkgs.lib; {
