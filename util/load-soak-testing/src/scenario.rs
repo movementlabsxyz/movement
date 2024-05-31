cf48c207dddcd523c873a791fa6e13970d743779
musitdev <philippe.delrieu@free.fr>
2024-05-31 17:19:48 +0200

    first version of the scenario executor.

--- util/load-soak-testing/src/scenario.rs
+++ util/load-soak-testing/src/scenario.rs
@@ -1,6 +1,7 @@
 use crate::TestExecutionError;
 use crate::EXEC_LOG_FILTER;
 
+<<<<<<< HEAD
 /// A scenario is any struct that implements the Scenario trait.
 /// To ease scenario execution login and using id (usize) is provided during creation.
 /// If you use it, it can be return after the scenario execution otherwise return any usize.
@@ -24,3 +25,25 @@ pub trait Scenario {
 
 /// Type definition that is used by the test executor to create scenario to execute.
 pub type CreateScenarioFn = (dyn Fn(usize) -> Box<dyn Scenario> + Send + Sync);
+=======
+pub struct Scenario {
+	id: usize,
+}
+
+impl Scenario {
+	pub fn new(id: usize) -> Self {
+		Scenario { id }
+	}
+
+	pub async fn run(self) -> (usize, Result<(), TestExecutionError>) {
+		tracing::info!("Scenarios:{} start", self.id);
+		let _ = tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
+		Self::log_exec_result(&format!("Scenario:{} ended", self.id));
+		(self.id, Ok(()))
+	}
+
+	fn log_exec_result(msg: &str) {
+		tracing::info!(target:EXEC_LOG_FILTER, msg);
+	}
+}
+>>>>>>> ff595882 (first version of the scenario executor.)
