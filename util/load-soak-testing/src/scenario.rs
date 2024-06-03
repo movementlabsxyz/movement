11569acecde3e92b0f334da83dbb13ee78da4339
musitdev <philippe.delrieu@free.fr>
2024-06-03 09:30:42 +0200

    first version of the Test runtime. Implements only Load test.

--- util/load-soak-testing/src/scenario.rs
+++ util/load-soak-testing/src/scenario.rs
@@ -1,23 +1,26 @@
 use crate::TestExecutionError;
 use crate::EXEC_LOG_FILTER;
 
-pub struct Scenario {
-	id: usize,
-}
+/// A scenario is any struct that implements the Scenario trait.
+/// To ease scenario execution login and using id (usize) is provided during creation.
+/// If you use it, it can be return after the scenario execution otherwise return any usize.
+/// This id is only use to identify scenario execution is the logs.
 
-impl Scenario {
-	pub fn new(id: usize) -> Self {
-		Scenario { id }
-	}
+/// Implements this trait to develop a scenario.
+/// Scenario execution logs:
+///  * on std out using tracing::{error, warn, info, debug, trace}
+///  * the same logs goes in the log file defined in the config.
+///  * to log in the execution json formated file use the log_exec_info function opf the trait.
+///
+///  Return the execution result (Result<(), TestExecutionError>) and a usize, id of the scenario or any usize.
+#[async_trait::async_trait]
+pub trait Scenario {
+	async fn run(self: Box<Self>) -> (usize, Result<(), TestExecutionError>);
 
-	pub async fn run(self) -> (usize, Result<(), TestExecutionError>) {
-		tracing::info!("Scenarios:{} start", self.id);
-		let _ = tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
-		Self::log_exec_result(&format!("Scenario:{} ended", self.id));
-		(self.id, Ok(()))
-	}
-
-	fn log_exec_result(msg: &str) {
+	fn log_exec_info(&self, msg: &str) {
 		tracing::info!(target:EXEC_LOG_FILTER, msg);
 	}
 }
+
+/// Type definition that is used by the test executor to create scenario to execute.
+pub type CreateScenarioFn = (dyn Fn(usize) -> Box<dyn Scenario> + Send + Sync);
