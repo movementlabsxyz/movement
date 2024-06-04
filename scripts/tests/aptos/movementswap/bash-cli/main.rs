c9f0d5bda74edf0d311aa2b6a74fcf8b19332824
Primata <primata@movementlabs.xyz>
2024-06-04 13:56:20 -0300

    appending demoscenario

--- scripts/tests/aptos/movementswap/bash-cli/main.rs
+++ scripts/tests/aptos/movementswap/bash-cli/main.rs
@@ -1,6 +1,9 @@
+
+use anyhow::Result;
 use load_soak_testing::execute_test;
 use load_soak_testing::init_test;
 use load_soak_testing::ExecutionConfig;
+use load_soak_testing::Scenario;
 
 fn main() {
 	println!("Initialize movementswap test...");
@@ -13,4 +16,35 @@ fn main() {
 	}
 	let result = execute_test(config);
 	tracing::info!("End Test with result {result:?}",);
+}
+
+fn create_movementswap_scenario(id: usize) -> Box<dyn Scenario> {
+	Box::new(ScenarioSwap { id })
+}
+
+pub struct ScenarioSwap {
+	id: usize,
+}
+
+impl ScenarioSwap {
+	pub fn new(id: usize) -> Self {
+		ScenarioSwap { id }
+	}
+}
+
+// Scenario trait implementation.
+#[async_trait::async_trait]
+impl Scenario for ScenarioSwap {
+	async fn run(self: Box<Self>) -> Result<()> {
+		// Trace in the log file and stdout.
+		tracing::info!("Scenarios:{} start", self.id);
+		let _ = tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
+		// Trace in the json formated execution log file.
+		self.log_exec_info(&format!("Scenario:{} ended", self.id));
+		Ok(())
+	}
+
+	fn get_id(&self) -> usize {
+		self.id
+	}
 }
