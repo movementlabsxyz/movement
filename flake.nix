a52b9f852277313d8ef4ece2d708aa72e6c9d1b2
Andy Bell <andyjsbell@gmail.com>
2024-06-07 15:25:52 +0200

    fix: test dependencies

--- flake.nix
+++ flake.nix
@@ -70,6 +70,9 @@
           just
           foundry-bin
           process-compose
+          celestia-node
+          celestia-app
+          monza-aptos
         ];
 
         # Specific version of toolchain
@@ -80,6 +83,30 @@
           rustc = rust;
         };
 
+        # Needs to be removed soon and replaced with aptos-faucet-service
+        monza-aptos = pkgs.stdenv.mkDerivation {
+            pname = "monza-aptos";
+            version = "branch-monza";
+
+            src = pkgs.fetchFromGitHub {
+                owner = "movementlabsxyz";
+                repo = "aptos-core";
+                rev = "06443b81f6b8b8742c4aa47eba9e315b5e6502ff";
+                sha256 = "sha256-iIYGbIh9yPtC6c22+KDi/LgDbxLEMhk4JJMGvweMJ1Q=";
+            };
+
+            installPhase = ''
+                cp -r . $out
+            '';
+
+            meta = with pkgs.lib; {
+                description = "Aptos core repository on the monza branch";
+                homepage = "https://github.com/movementlabsxyz/aptos-core";
+                license = licenses.asl20;
+            };
+        };
+        # Remember, remove this thing above
+        
         # celestia-node
         celestia-node = import ./nix/celestia-node.nix { inherit pkgs; };
 
