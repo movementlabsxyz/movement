991713ef936ff7247cf825550406efbe786c798b
Liam Monninger <l.mak.monninger@gmail.com>
2024-06-08 10:11:19 +0200

    fix: paused gas metering.

--- protocol-units/settlement/mcr/contracts/test/settlement/MCR.sol
+++ protocol-units/settlement/mcr/contracts/test/settlement/MCR.sol
@@ -294,4 +294,151 @@ contract MCRTest is Test {
         );
         assert(mcr.getAcceptedCommitmentAtBlockHeight(1).height == 1);
     }
+
+    address[] honestSigners = new address[](0);
+    address[] dishonestSigners = new address[](0);
+
+    function testChangingValidatorSet() public {
+        vm.pauseGasMetering();
+
+        uint256 blockTime = 300;
+        MOVEToken moveToken = new MOVEToken();
+        moveToken.initialize();
+
+        MovementStaking staking = new MovementStaking();
+        staking.initialize(moveToken);
+
+        MCR mcr = new MCR();
+        address[] memory custodians = new address[](1);
+        custodians[0] = address(moveToken);
+        mcr.initialize(staking, 0, 5, 10 seconds, custodians);
+
+        vm.warp(blockTime);
+
+        // three well-funded signers
+        address payable alice = payable(vm.addr(1));
+        moveToken.mint(alice, 100);
+        address payable bob = payable(vm.addr(2));
+        moveToken.mint(bob, 100);
+        address payable carol = payable(vm.addr(3));
+        moveToken.mint(carol, 100);
+
+        // have them participate in the genesis ceremony
+        vm.prank(alice);
+        moveToken.approve(address(staking), 100);
+        vm.prank(alice);
+        staking.stake(address(mcr), moveToken, 34);
+        vm.prank(bob);
+        moveToken.approve(address(staking), 100);
+        vm.prank(bob);
+        staking.stake(address(mcr), moveToken, 33);
+        vm.prank(carol);
+        moveToken.approve(address(staking), 100);
+        vm.prank(carol);
+        staking.stake(address(mcr), moveToken, 33);
+
+        // honest signers
+        honestSigners.push(alice);
+        honestSigners.push(bob);
+
+        // dishonest signers
+        dishonestSigners.push(carol);
+
+        uint reorgs = 50;
+        for (uint i = 0; i < reorgs; i++) {
+            uint commitmentHeights = 10;
+            for (uint j = 0; j < commitmentHeights; j++) {
+                uint256 blockHeight = i * 10 + j + 1;
+                blockTime += 1;
+                vm.warp(blockTime);
+
+                // commit dishonestly
+                MCRStorage.BlockCommitment
+                    memory dishonestCommitment = MCRStorage.BlockCommitment({
+                        height: blockHeight,
+                        commitment: keccak256(
+                            abi.encodePacked(uint256(3), uint256(2), uint256(1))
+                        ),
+                        blockId: keccak256(
+                            abi.encodePacked(uint256(3), uint256(2), uint256(1))
+                        )
+                    });
+                for (uint k = 0; k < dishonestSigners.length / 2; k++) {
+                    vm.prank(dishonestSigners[k]);
+                    mcr.submitBlockCommitment(dishonestCommitment);
+                }
+
+                // commit honestly
+                MCRStorage.BlockCommitment memory honestCommitment = MCRStorage
+                    .BlockCommitment({
+                        height: blockHeight,
+                        commitment: keccak256(
+                            abi.encodePacked(uint256(1), uint256(2), uint256(3))
+                        ),
+                        blockId: keccak256(
+                            abi.encodePacked(uint256(1), uint256(2), uint256(3))
+                        )
+                    });
+                for (uint k = 0; k < honestSigners.length; k++) {
+                    vm.prank(honestSigners[k]);
+                    mcr.submitBlockCommitment(honestCommitment);
+                }
+
+                // commit dishonestly some more
+                for (
+                    uint k = dishonestSigners.length / 2;
+                    k < dishonestSigners.length;
+                    k++
+                ) {
+                    vm.prank(dishonestSigners[k]);
+                    mcr.submitBlockCommitment(dishonestCommitment);
+                }
+
+                MCR.BlockCommitment memory acceptedCommitment = mcr
+                    .getAcceptedCommitmentAtBlockHeight(blockHeight);
+                assert(
+                    acceptedCommitment.commitment == honestCommitment.commitment
+                );
+                assert(acceptedCommitment.blockId == honestCommitment.blockId);
+                assert(acceptedCommitment.height == blockHeight);
+            }
+
+            // add a new signer
+            address payable newSigner = payable(vm.addr(4 + i));
+            moveToken.mint(newSigner, 100);
+            vm.prank(newSigner);
+            moveToken.approve(address(staking), 33);
+            vm.prank(newSigner);
+            staking.stake(address(mcr), moveToken, 33);
+
+            if (i % 3 == 2) {
+                dishonestSigners.push(newSigner);
+            } else {
+                honestSigners.push(newSigner);
+            }
+
+            if (i % 5 == 4) {
+                // remove a dishonest signer
+                address dishonestSigner = dishonestSigners[0];
+                vm.prank(dishonestSigner);
+                staking.unstake(address(mcr), address(moveToken), 33);
+                dishonestSigners[0] = dishonestSigners[
+                    dishonestSigners.length - 1
+                ];
+                dishonestSigners.pop();
+            }
+
+            if (i % 8 == 7) {
+                // remove an honest signer
+                address honestSigner = honestSigners[0];
+                vm.prank(honestSigner);
+                staking.unstake(address(mcr), address(moveToken), 33);
+                honestSigners[0] = honestSigners[honestSigners.length - 1];
+                honestSigners.pop();
+            }
+
+            blockTime += 5;
+            vm.warp(blockTime);
+        }
+    }
 }
