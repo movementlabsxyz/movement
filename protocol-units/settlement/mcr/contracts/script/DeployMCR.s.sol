06b3c3c8444ddf7e81c18543622ebb3a9bcd208e
Primata <primata@movementlabs.xyz>
2024-06-11 12:35:02 +0200

    pre pull MCR upgradeability

--- protocol-units/settlement/mcr/contracts/script/DeployMCR.s.sol
+++ protocol-units/settlement/mcr/contracts/script/DeployMCR.s.sol
@@ -1,16 +1,25 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "../src/MCR.sol";
-import {ProxyAdmin} from "@openzeppelin/contracts/proxy/ProxyAdmin.sol";
-import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/TransparentUpgradeableProxy.sol";
-import {TimeLockController} from "@openzeppelin/contracts/access/TimeLockController.sol";
+import {MCR} from "../src/MCR.sol";
+import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+
+function string2Address(bytes memory str) returns (address addr) {
+    bytes32 data = keccak256(str);
+        assembly {
+        mstore(0, data)
+        addr := mload(0)
+    }
+    
+}
 
 contract DeployMCR is Script {
 
     TransparentUpgradeableProxy public mcrProxy;
     ProxyAdmin public admin;
-    TimeLock public timeLock;
+    TimelockController public timelock;
     string public signature = "initialize(uint256,uint256,uint256,uint256,uint256)";
 
 
@@ -19,21 +28,21 @@ contract DeployMCR is Script {
         address[] memory proposers = new address[](5);
         address[] memory executors = new address[](1);
 
-        proposers[0] = address(keccak256("Andy"));
-        proposers[1] = address(keccak256("Bob"));
-        proposers[2] = address(keccak256("Charlie"));
-        proposers[3] = address(keccak256("David"));
-        proposers[4] = address(keccak256("Eve"));
+        proposers[0] = address(string2Address("Andy"));
+        proposers[1] = address(string2Address("Bob"));
+        proposers[2] = address(string2Address("Charlie"));
+        proposers[3] = address(string2Address("David"));
+        proposers[4] = address(string2Address("Eve"));
 
-        executors[0] = address(keccak256("MultisigAddress"));
+        executors[0] = address(string2Address("MultisigAddress"));
 
-        address adminAddress = 0x0;
+        address adminAddress = address(0);
 
         vm.startBroadcast();
         
         MCR mcrImplementation = new MCR();
 
-        admin = new ProxyAdmin();
+        admin = new ProxyAdmin(address(this));
         mcrProxy = new TransparentUpgradeableProxy(address(mcrImplementation), address(admin), abi.encodeWithSignature(signature,
             5, 
             128,
@@ -42,16 +51,16 @@ contract DeployMCR is Script {
             0
         ));
 
-        timeLock = new TimelockController(minDelay, proposers, executors, adminAddress);
-        admin.transferOwnership(address(timeLock));
+        timelock = new TimelockController(minDelay, proposers, executors, adminAddress);
+        admin.transferOwnership(address(timelock));
 
         MCR mcrImplementation2 = new MCR();
         vm.stopBroadcast();
         // deploy a new implementation of MCR and schedule an upgrade
-        vm.startBroadcast(vm.envUint("ANDY_PRIVATE_KEY"););
+        vm.startBroadcast(vm.envUint("ANDY_PRIVATE_KEY"));
         address to = address(mcrProxy);
         uint256 value = 0; // not sure
-        bytes payload = abi.encodeWithSignature("upgradeTo(address)", address(mcrImplementation2));
+        bytes memory payload = abi.encodeWithSignature("upgradeTo(address)", address(mcrImplementation2));
         bytes32 predecessor = bytes32(0); // not sure
         bytes32 salt = bytes32(0); // not sure
         uint256 delay = 1 days + 1;
