913cf295856cefdb41d07b5d362aa3f68c807831
Primata <primata@movementlabs.xyz>
2024-06-11 14:01:01 +0200

    Deploy move and stlMove

--- protocol-units/settlement/mcr/contracts/script/DeployMoveStlMove.s.sol
+++ protocol-units/settlement/mcr/contracts/script/DeployMoveStlMove.s.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import {MintableToken} from "../src/toke/base/MintableToken.sol";
+import {stlMoveToken, IMintableToken} from "../src/token/stlMoveToken.sol";
 import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
@@ -20,7 +21,8 @@ contract DeployMintableToken is Script {
     TransparentUpgradeableProxy public moveProxy;
     ProxyAdmin public admin;
     TimelockController public timelock;
-    string public signature = "initialize(string,string)";
+    string public moveSignature = "initialize(string,string)";
+    string public stlSignature = "initialize(IMintableToken)";
 
 
     function run() external {
@@ -41,14 +43,19 @@ contract DeployMintableToken is Script {
         vm.startBroadcast();
         
         MintableToken moveImplementation = new MintableToken();
+        stlMoveToken stlMoveImplementation = new stlMoveToken();
 
         admin = new ProxyAdmin(address(this));
         moveProxy = new TransparentUpgradeableProxy(address(moveImplementation), address(admin), abi.encodeWithSignature(signature,
             "Move Token",
             "MOVE"
         ));
+        stlMoveProxy = new TransparentUpgradeableProxy(address(stlMoveImplementation), address(admin), abi.encodeWithSignature(signature,
+            IMintableToken(moveProxy)
+        ));
 
         timelock = new TimelockController(minDelay, proposers, executors, adminAddress);
+        // since admin proxy owns both move and stlmove, we only need to transfer ownership of admin to timelock
         admin.transferOwnership(address(timelock));
 
         MintableToken moveImplementation2 = new MintableToken();
