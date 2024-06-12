af7e66039243391cc8bf3fe33820db3f45964e25
Code8 <code8@movementlabs.xyz>
2024-06-12 21:25:48 +0200

    feat: implement stream on mock + test
    Why:
    - Added the ability to use streams in the `BlockchainService` trait and its associated types `BlockchainEvent`.
    - Added implementation of the `Stream` trait for `MockBlockchainService` in the test file.
    
    Changes:
    - Modified `types.rs` to derive `PartialEq` and `Eq` for the structs.

--- protocol-units/bridge/shared/src/bridge_monitoring.rs
+++ protocol-units/bridge/shared/src/bridge_monitoring.rs
@@ -2,12 +2,14 @@ use futures::Stream;
 
 use crate::types::{BridgeTransferDetails, BridgeTransferId};
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum BridgeContractInitiatorEvent<A, H> {
 	BridgeTransferInitiated(BridgeTransferDetails<A, H>),
 	BridgeTransferCompleted(BridgeTransferId<H>),
 	BridgeTransferRefunded(BridgeTransferId<H>),
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum BridgeContractCounterpartyEvent<A, H> {
 	BridgeTransferLocked(BridgeTransferDetails<A, H>),
 }
