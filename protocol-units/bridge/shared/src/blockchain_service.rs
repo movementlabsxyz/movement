935f80a250a113b5ec1978cd4f701fdd8d43ae6f
Code8 <code8@movementlabs.xyz>
2024-06-12 22:27:19 +0200

    refactor: dry poll_next_event
    Implementing the `poll_next_event` function in the `BlockchainService` trait in order to avoid code duplication and improve code readability.

--- protocol-units/bridge/shared/src/blockchain_service.rs
+++ protocol-units/bridge/shared/src/blockchain_service.rs
@@ -1,4 +1,9 @@
-use futures::Stream;
+use std::{
+	pin::Pin,
+	task::{Context, Poll},
+};
+
+use futures::{Stream, StreamExt};
 
 use crate::{
 	bridge_contracts::{BridgeContractCounterparty, BridgeContractInitiator},
@@ -21,19 +26,35 @@ pub trait BlockchainService:
 	type Hash;
 
 	type InitiatorContract: BridgeContractInitiator;
-	type InitiatorMonitoring: BridgeContractInitiatorMonitoring<
-		Address = Self::Address,
-		Hash = Self::Hash,
-	>;
+	type InitiatorMonitoring: BridgeContractInitiatorMonitoring<Address = Self::Address, Hash = Self::Hash>
+		+ Unpin;
 
 	type CounterpartyContract: BridgeContractCounterparty;
-	type CounterpartyMonitoring: BridgeContractCounterpartyMonitoring<
-		Address = Self::Address,
-		Hash = Self::Hash,
-	>;
+	type CounterpartyMonitoring: BridgeContractCounterpartyMonitoring<Address = Self::Address, Hash = Self::Hash>
+		+ Unpin;
 
 	fn initiator_contract(&self) -> &Self::InitiatorContract;
-	fn initiator_monitoring(&self) -> &Self::InitiatorMonitoring;
+	fn initiator_monitoring(&mut self) -> &mut Self::InitiatorMonitoring;
 	fn counterparty_contract(&self) -> &Self::CounterpartyContract;
-	fn counterparty_monitoring(&self) -> &Self::CounterpartyMonitoring;
+	fn counterparty_monitoring(&mut self) -> &mut Self::CounterpartyMonitoring;
+
+	fn poll_next_event(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+		let this = self.get_mut();
+
+		// let initiator_monitoring = Pin::new(&mut this.initiator_monitoring);
+		// let counterparty_monitoring = Pin::new(&mut this.counterparty_monitoring);
+
+		match (
+			this.initiator_monitoring().poll_next_unpin(cx),
+			this.counterparty_monitoring().poll_next_unpin(cx),
+		) {
+			(Poll::Ready(Some(event)), _) => {
+				Poll::Ready(Some(BlockchainEvent::InitiatorEvent(event)))
+			}
+			(_, Poll::Ready(Some(event))) => {
+				Poll::Ready(Some(BlockchainEvent::CounterpartyEvent(event)))
+			}
+			_ => Poll::Pending,
+		}
+	}
 }
