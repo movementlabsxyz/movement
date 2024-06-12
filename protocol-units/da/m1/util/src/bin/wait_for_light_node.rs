388ff3eddf3faf09006fede738cbe17873bc5aca
Liam Monninger <l.mak.monninger@gmail.com>
2024-06-12 15:45:40 +0200

    fix: adding waits.

--- protocol-units/da/m1/util/src/bin/wait_for_light_node.rs
+++ protocol-units/da/m1/util/src/bin/wait_for_light_node.rs
@@ -4,6 +4,14 @@ use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+
+	use tracing_subscriber::EnvFilter;
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info")))
+        .init();
+
 	let dot_movement = dot_movement::DotMovement::try_from_env()?;
 	let path = dot_movement.get_path().join("config.toml");
 	let config = Config::try_from_toml_file(&path).unwrap_or_default();
