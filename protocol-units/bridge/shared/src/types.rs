1224198ddc9e10a2d2383542ab74f9924e11c217
Code8 <code8@movementlabs.xyz>
2024-06-12 22:08:15 +0200

    refactor: adding wrapper types for compile time checking
    Added wrapper types HashLock, InitiatorAddress, RecipientAddress, TimeLock for better compile-time type checking

--- protocol-units/bridge/shared/src/types.rs
+++ protocol-units/bridge/shared/src/types.rs
@@ -3,12 +3,24 @@ use derive_more::Deref;
 #[derive(Deref, Debug, PartialEq, Eq)]
 pub struct BridgeTransferId<H>(pub H);
 
+#[derive(Deref, Debug, PartialEq, Eq)]
+pub struct InitiatorAddress<A>(pub A);
+
+#[derive(Deref, Debug, PartialEq, Eq)]
+pub struct RecipientAddress<A>(pub A);
+
+#[derive(Deref, Debug, PartialEq, Eq)]
+pub struct HashLock<H>(pub H);
+
+#[derive(Deref, Debug, PartialEq, Eq)]
+pub struct TimeLock(pub u64);
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct BridgeTransferDetails<A, H> {
 	pub bridge_transfer_id: BridgeTransferId<H>,
-	pub initiator_address: A,
-	pub recipient_address: A,
-	pub hash_lock: H,
-	pub time_lock: u64,
+	pub initiator_address: InitiatorAddress<A>,
+	pub recipient_address: RecipientAddress<A>,
+	pub hash_lock: HashLock<H>,
+	pub time_lock: TimeLock,
 	pub amount: u64,
 }
