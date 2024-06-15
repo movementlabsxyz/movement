ad9710548a4930b443a7fcb2d44ea4b4265365df
Code8 <code8@movementlabs.xyz>
2024-06-15 02:35:33 +0200

    feat: Made necessary changes to support transformations in bridge service and contracts
    Changes:
    - Updated types in bridge contracts to utilize BridgeAddressType and BridgeHashType for Address and Hash types respectively.
    - Included Send and Sync traits in BridgeContractCounterparty trait for transformations support.
    - Updated BridgeService to use specific types for active swaps to facilitate transformations.

--- protocol-units/bridge/shared/src/bridge_service/active_swap.rs
+++ protocol-units/bridge/shared/src/bridge_service/active_swap.rs
@@ -1,53 +1,120 @@
-use std::{collections::HashMap, pin::Pin};
+use std::{collections::HashMap, convert::From, pin::Pin};
 
-use futures::Future;
+use futures::{Future, FutureExt};
+use tracing::{trace_span, Instrument};
 
-use crate::types::{BridgeAddressType, BridgeHashType, BridgeTransferDetails, BridgeTransferId};
+use crate::{
+	blockchain_service::BlockchainService,
+	types::{BridgeTransferDetails, BridgeTransferId, HashLock},
+};
+use crate::{bridge_contracts::BridgeContractCounterparty, types::RecipientAddress};
 
 pub type BoxedBridgeServiceFuture = Pin<Box<dyn Future<Output = ()> + Send>>;
 
 /// Bridge state in the sense of tracking the active swaps from the bridge
-pub enum ActiveSwap<BFromA, BFromH> {
+pub enum ActiveSwap<BFrom, BTo>
+where
+	BFrom: BlockchainService,
+	BTo: BlockchainService,
+{
 	/// Bridge is locking lockens on the counterpart chain
-	LockingTokens(BridgeTransferDetails<BFromA, BFromH>, BoxedBridgeServiceFuture),
+	LockingTokens(BridgeTransferDetails<BFrom::Address, BFrom::Hash>, BoxedBridgeServiceFuture),
 	/// Bridge is waiting for the initiator to complete her transfer
 	/// revealing her secret.
-	WaitingForCompletedEvent(BridgeTransferId<BFromH>),
+	WaitingForCompletedEvent(BridgeTransferId<BTo::Hash>),
 	/// We are in possession of the secret and are now completing the bridge tranfer on our side
 	CompletingBridging(BoxedBridgeServiceFuture),
 	/// We have completed the atomic bdridge transfer
 	Completed,
 }
 
-pub struct ActiveSwapMap<BFromA, BFromH, BFromCI, BToCC> {
-	pub initiator_contract: BFromCI,
-	pub counterparty_contract: BToCC,
-	swaps: HashMap<BridgeTransferId<BFromH>, ActiveSwap<BFromA, BFromH>>,
+pub struct ActiveSwapMap<BFrom, BTo>
+where
+	BFrom: BlockchainService,
+	BTo: BlockchainService,
+{
+	pub initiator_contract: BFrom::InitiatorContract,
+	pub counterparty_contract: BTo::CounterpartyContract,
+	swaps: HashMap<BridgeTransferId<BFrom::Hash>, ActiveSwap<BFrom, BTo>>,
 }
 
-impl<BFromA, BFromH, BFromCI, BToCC> ActiveSwapMap<BFromA, BFromH, BFromCI, BToCC>
+impl<BTo, BFrom> ActiveSwapMap<BFrom, BTo>
 where
-	BFromH: BridgeHashType,
-	BFromA: BridgeAddressType,
+	BTo: BlockchainService + 'static,
+	BFrom: BlockchainService + 'static,
 {
-	pub fn build(initiator_contract: BFromCI, counterparty_contract: BToCC) -> Self {
+	pub fn build(
+		initiator_contract: BFrom::InitiatorContract,
+		counterparty_contract: BTo::CounterpartyContract,
+	) -> Self {
 		Self { initiator_contract, counterparty_contract, swaps: HashMap::new() }
 	}
 
-	pub fn get(&self, key: &BridgeTransferId<BFromH>) -> Option<&ActiveSwap<BFromA, BFromH>> {
+	pub fn get(&self, key: &BridgeTransferId<BFrom::Hash>) -> Option<&ActiveSwap<BFrom, BTo>> {
 		self.swaps.get(key)
 	}
 
-	pub fn already_executing(&self, key: &BridgeTransferId<BFromH>) -> bool {
+	pub fn already_executing(&self, key: &BridgeTransferId<BFrom::Hash>) -> bool {
 		self.swaps.contains_key(key)
 	}
 
-	pub fn start(&mut self, details: BridgeTransferDetails<BFromA, BFromH>) {
+	pub fn start(&mut self, details: BridgeTransferDetails<BFrom::Address, BFrom::Hash>)
+	where
+		<BTo::CounterpartyContract as BridgeContractCounterparty>::Hash: From<BFrom::Hash>,
+		<BTo::CounterpartyContract as BridgeContractCounterparty>::Address: From<BFrom::Address>,
+	{
 		assert!(self.swaps.get(&details.bridge_transfer_id).is_none());
 
+		let counterparty_contract = self.counterparty_contract.clone();
+		let bridge_transfer_id = details.bridge_transfer_id.clone();
+
+		tracing::trace!("Starting active swap for bridge transfer {:?}", bridge_transfer_id);
+
 		self.swaps.insert(
-			details.bridge_transfer_id.clone(),
-			ActiveSwap::LockingTokens(details, Box::pin(async move {})),
+			bridge_transfer_id,
+			ActiveSwap::<BFrom, BTo>::LockingTokens(
+				details.clone(),
+				call_lock_bridge_transfer_assets::<BFrom, BTo>(counterparty_contract, details)
+					.instrument(trace_span!(
+						"call_lock_bridge_transfer_assets[{bridge_transfer_id:?}]"
+					))
+					.boxed(),
+			),
 		);
 	}
+}
+
+/// Making sure we call the method using the correct details on the counterparty contract
+async fn call_lock_bridge_transfer_assets<BFrom: BlockchainService, BTo: BlockchainService>(
+	counterparty_contract: BTo::CounterpartyContract,
+	BridgeTransferDetails {
+		bridge_transfer_id,
+		hash_lock,
+		time_lock,
+		recipient_address,
+		amount,
+		..
+	}: BridgeTransferDetails<BFrom::Address, BFrom::Hash>,
+) where
+	<BTo::CounterpartyContract as BridgeContractCounterparty>::Hash: From<BFrom::Hash>,
+	<BTo::CounterpartyContract as BridgeContractCounterparty>::Address: From<BFrom::Address>,
+{
+	let bridge_transfer_id = BridgeTransferId(From::from(bridge_transfer_id.0));
+	let hash_lock = HashLock(From::from(hash_lock.0));
+	let recipient_address = RecipientAddress(From::from(recipient_address.0));
+
+	tracing::trace!(
+		"Calling lock_bridge_transfer_assets on counterparty contract for bridge transfer {:?}",
+		bridge_transfer_id
+	);
+
+	counterparty_contract
+		.lock_bridge_transfer_assets(
+			bridge_transfer_id,
+			hash_lock,
+			time_lock,
+			recipient_address,
+			amount,
+		)
+		.await;
 }
