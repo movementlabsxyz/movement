426ffc1de1e88190c093b752b26a731cb1f49803
Richard Melkonian <r.v.melkonian@gmail.com>
2024-06-27 22:15:43 +0100

    alloy things

--- protocol-units/bridge/chains/ethereum/src/lib.rs
+++ protocol-units/bridge/chains/ethereum/src/lib.rs
@@ -1,8 +1,12 @@
-use alloy::providers::{Proverder, ProviderBuilder};
-use alloy::{node_bindings::Anvil, providers::ProviderBuilder, sol};
 use alloy_primitives::{address, Address as EthAddress};
 use anyhow::anyhow;
-use bridge_shared::bridge_contracts::BridgeContractInitiator;
+use alloy::{
+	network::EthereumWallet, node_bindings::Anvil, primitives::U256, providers::ProviderBuilder,
+	signers::local::PrivateKeySigner, sol,
+};
+use alloy_provider
+use bridge_shared::bridge_contracts::{BridgeContractInitiator, BridgeContractResult};
+use bridge_shared::types::{Amount, BridgeTransferDetails, BridgeTransferId, HashLock, HashLockPreImage, InitiatorAddress, RecipientAddress, TimeLock};
 
 //Codegen the ABI file
 sol!("../../../contracts/out/AtomicBridgeInitator.sol/AtomicBridgeInitiator.json",);
@@ -15,17 +19,25 @@ pub struct EthClient {
 	initiator: AtomicBridgeInitiator,
 }
 
+struct AtomicBridgeInitiator(_, ReqwestProvider);
+
 impl EthClient {
 	pub fn build(
 		url: &str,
 		initiator_address: &str,
 		counterparty_address: &str,
 	) -> Result<Self, anyhow::Error> {
-		let anvil = Anvil::new().fork(rpc_url).try_spawn()?;
+		let anvil = Anvil::new().fork(url).try_spawn()?;
 		let rpc_url = anvil.rpc_url()?;
 		let provider = ProviderBuilder::new().on_http(rpc_url);
 		let initiator = AtomicBridgeInitiator::new(initiator_address.parse()?, provider);
 		Ok(Self { provider, anvil, initiator })
+	}
+}
+
+impl Clone for EthClient {
+	fn clone(&self) -> Self {
+		todo!()
 	}
 }
 
@@ -41,7 +53,9 @@ impl BridgeContractInitiator for EthClient {
 		time_lock: TimeLock,
 		amount: Amount,
 	) -> BridgeContractResult<()> {
-		self.initiator
+		// do we for sure not want to do anything with the return from the contract?
+		let _res = self
+			.initiator
 			.initiateBridgeTransfer(amount, recipient_address, hash_lock, time_lock)
 			.await?;
 		Ok(())
@@ -50,8 +64,9 @@ impl BridgeContractInitiator for EthClient {
 	async fn complete_bridge_transfer(
 		&mut self,
 		bridge_transfer_id: BridgeTransferId<Self::Hash>,
-		secret: HashLockPreImage,
+		pre_image: HashLockPreImage,
 	) -> BridgeContractResult<()> {
+		let _res = self.initiator.completeBridgeTransfer(bridge_transfer_id, pre_image).await?;
 		Ok(())
 	}
 
