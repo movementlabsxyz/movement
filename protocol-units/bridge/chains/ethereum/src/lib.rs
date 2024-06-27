f8063ec87afc5047ae4b2fec3888c7be0b5ccde6
Richard Melkonian <r.v.melkonian@gmail.com>
2024-06-27 19:46:38 +0100

    merge in ETH contracts

--- protocol-units/bridge/chains/ethereum/src/lib.rs
+++ protocol-units/bridge/chains/ethereum/src/lib.rs
@@ -1,5 +1,56 @@
+use alloy::providers::{Proverder, ProviderBuilder};
+use bridge_shared::bridge_contracts::BridgeContractInitiator;
+
 pub struct EthHash(); // Alloy type inside
-					  //
-struct EthInitiatorContractMonitoring<A, H> {
-	listener: UnboundedReceiver<AbstractBlockchainEvent<A, H>>,
+
+pub struct EthClient {
+	provider: Provider,
 }
+
+impl EthClient {
+		pub fn build(rpc_url: &str) -> Self {
+				let provider = ProviderBuilder::new().rpc_url(rpc_url).build();
+				Self { provider }
+		}
+}
+
+impl BridgeContractInitiator for EthClient {
+		type Address = EthAddress;
+		type Hash = EthHash;
+
+		async fn initiate_bridge_transfer(
+			&mut self,
+			initiator_address: InitiatorAddress<Self::Address>,
+			recipient_address: RecipientAddress<Self::Address>,
+			hash_lock: HashLock<Self::Hash>,
+			time_lock: TimeLock,
+			amount: Amount,
+		) -> BridgeContractResult<()> {
+			Ok(())
+		}
+
+		async fn complete_bridge_transfer(
+			&mut self,
+			bridge_transfer_id: BridgeTransferId<Self::Hash>,
+			secret: HashLockPreImage,
+		) -> BridgeContractResult<()> {
+			Ok(())
+		}
+
+		async fn refund_bridge_transfer(
+			&mut self,
+			bridge_transfer_id: BridgeTransferId<Self::Hash>,
+		) -> BridgeContractResult<()> {
+			Ok(())
+		}
+
+		async fn get_bridge_transfer_details(
+			&mut self,
+			bridge_transfer_id: BridgeTransferId<Self::Hash>,
+		) -> BridgeContractResult<Option<BridgeTransferDetails<Self::Hash, Self::Address>> {
+			Ok(None)
+		}
+}
+
+
+
