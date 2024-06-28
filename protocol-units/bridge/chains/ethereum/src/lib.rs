97bd1494761ae284c186b45dfb85e80ebe183b43
Richard Melkonian <r.v.melkonian@gmail.com>
2024-06-28 13:10:31 +0100

    complete rpc calls, handle type conversion

--- protocol-units/bridge/chains/ethereum/src/lib.rs
+++ protocol-units/bridge/chains/ethereum/src/lib.rs
@@ -1,7 +1,7 @@
 use alloy::pubsub::PubSubFrontend;
 use alloy_network::{Ethereum, EthereumSigner};
 use alloy_primitives::private::serde::{Deserialize, Serialize};
-use alloy_primitives::{address, Address as EthAddress};
+use alloy_primitives::{Address as EthAddress, FixedBytes, U256};
 use alloy_provider::{
 	fillers::{ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller, SignerFiller},
 	Provider, ProviderBuilder, RootProvider,
@@ -10,8 +10,9 @@ use alloy_signer_wallet::LocalWallet;
 use alloy_sol_types::sol;
 use alloy_transport::BoxTransport;
 use alloy_transport_ws::WsConnect;
-use anyhow::Context;
-use bridge_shared::bridge_contracts::{BridgeContractInitiator, BridgeContractResult};
+use bridge_shared::bridge_contracts::{
+	BridgeContractError, BridgeContractInitiator, BridgeContractResult,
+};
 use bridge_shared::types::{
 	Amount, BridgeTransferDetails, BridgeTransferId, HashLock, HashLockPreImage, InitiatorAddress,
 	RecipientAddress, TimeLock,
@@ -19,6 +20,8 @@ use bridge_shared::types::{
 use mcr_settlement_client::send_eth_tx::{
 	send_tx, InsufficentFunds, SendTxErrorRule, UnderPriced, VerifyRule,
 };
+
+use anyhow::Context;
 
 const INITIATOR_ADDRESS: &str = "0xinitiator";
 const COUNTERPARTY_ADDRESS: &str = "0xcounter";
@@ -30,8 +33,8 @@ const MAX_RETRIES: u32 = 5;
 pub struct Config {
 	pub rpc_url: Option<String>,
 	pub ws_url: Option<String>,
-	pub signer_private_key: Option<String>,
-	pub initiator_address: Option<String>,
+	pub signer_private_key: String,
+	pub initiator_address: String,
 	pub counterparty_address: Option<String>,
 	pub gas_limit: u64,
 	pub num_tx_send_retries: u32,
@@ -42,7 +45,7 @@ impl Default for Config {
 		Config {
 			rpc_url: Some("http://localhost:8545".to_string()),
 			ws_url: Some("ws://localhost:8546".to_string()),
-			signer_private_key: Some(LocalWallet::random().to_bytes().to_string()),
+			signer_private_key: LocalWallet::random().to_bytes().to_string(),
 			initiator_address: Some(INITIATOR_ADDRESS.to_string()),
 			counterparty_address: Some(COUNTERPARTY_ADDRESS.to_string()),
 			gas_limit: DEFAULT_GAS_LIMIT,
@@ -72,13 +75,14 @@ type AlloyProvider = FillProvider<
 	Ethereum,
 >;
 
-pub struct EthHash(); // Alloy type inside
+#[derive(Debug, Hash, Eq, PartialEq, Clone, Serialize, Deserialize)]
+pub struct EthHash([u8; 32]);
 
 pub struct EthClient<P> {
 	rpc_provider: P,
 	ws_provider: RootProvider<PubSubFrontend>,
-	initiator_address: Option<EthAddress>,
-	counterparty_address: Option<EthAddress>,
+	initiator_address: EthAddress,
+	counterparty_address: EthAddress,
 	send_tx_error_rules: Vec<Box<dyn VerifyRule>>,
 	gas_limit: u64,
 	num_tx_send_retries: u32,
@@ -89,7 +93,7 @@ impl EthClient<AlloyProvider> {
 		config: Config,
 		counterparty_address: &str,
 	) -> Result<Self, anyhow::Error> {
-		let signer_private_key = config.signer_private_key.context("signer_private_key not set")?;
+		let signer_private_key = config.signer_private_key;
 		let signer: LocalWallet = signer_private_key.parse()?;
 		let initiator_address = config.initiator_address.parse()?;
 		let rpc_url = config.rpc_url.context("rpc_url not set")?;
@@ -102,10 +106,10 @@ impl EthClient<AlloyProvider> {
 
 		EthClient::build_with_provider(
 			rpc_provider,
-			RootProvider::new(ws_url),
+			ws_url,
 			initiator_address,
-			Some(counterparty_address.parse()?),
-			Some(counterparty_address.parse()?),
+			counterparty_address.parse()?,
+			counterparty_address.parse()?,
 			config.gas_limit,
 			config.num_tx_send_retries,
 		)
@@ -114,10 +118,10 @@ impl EthClient<AlloyProvider> {
 
 	async fn build_with_provider<S>(
 		rpc_provider: AlloyProvider,
-		ws_provider: RootProvider<S>,
+		ws_provider: S,
 		signer_address: EthAddress,
-		initiator_address: Option<EthAddress>,
-		counterparty_address: Option<EthAddress>,
+		initiator_address: EthAddress,
+		counterparty_address: EthAddress,
 		gas_limit: u64,
 		num_tx_send_retries: u32,
 	) -> Result<Self, anyhow::Error>
@@ -143,18 +147,19 @@ impl EthClient<AlloyProvider> {
 	}
 }
 
-impl Clone for EthClient {
+impl<P> Clone for EthClient<P> {
 	fn clone(&self) -> Self {
 		todo!()
 	}
 }
 
+#[async_trait::async_trait]
 impl<P> BridgeContractInitiator for EthClient<P>
 where
-	P: Provider + Clone,
+	P: Provider + Clone + Send + Sync + Unpin,
 {
-	type Address = EthAddress;
-	type Hash = EthHash;
+	type Address = [u8; 32];
+	type Hash = [u8; 32];
 
 	async fn initiate_bridge_transfer(
 		&mut self,
@@ -164,11 +169,13 @@ where
 		time_lock: TimeLock,
 		amount: Amount,
 	) -> BridgeContractResult<()> {
-		let contract =
-			AtomicBridgeInitiator::new(self.initiator_address, self.rpc_provider.clone());
-		let call = contract
-			.initiateBridgeTransfer(amount, recipient_address, hash_lock, time_lock)
-			.await?;
+		let contract = AtomicBridgeInitiator::new(self.initiator_address, &self.rpc_provider);
+		let call = contract.initiateBridgeTransfer(
+			U256::from(amount.0),
+			FixedBytes(recipient_address.0),
+			FixedBytes(hash_lock.0),
+			U256::from(time_lock.0),
+		);
 		send_tx(call, &self.send_tx_error_rules, self.num_tx_send_retries, self.gas_limit as u128)
 			.await;
 		Ok(())
@@ -179,9 +186,12 @@ where
 		bridge_transfer_id: BridgeTransferId<Self::Hash>,
 		pre_image: HashLockPreImage,
 	) -> BridgeContractResult<()> {
+		let pre_image: [u8; 32] =
+			vec_to_array(pre_image.0).unwrap_or_else(|_| panic!("Failed to convert pre_image"));
 		let contract =
-			AtomicBridgeInitiator::new(self.initiator_address, self.rpc_provider.clone());
-		let call = contract.completeBridgeTransfer(bridge_transfer_id, pre_image).await?;
+			AtomicBridgeInitiator::new(self.initiator_address, &self.rpc_provider.clone());
+		let call = contract
+			.completeBridgeTransfer(FixedBytes(bridge_transfer_id.0), FixedBytes(pre_image));
 		send_tx(call, &self.send_tx_error_rules, self.num_tx_send_retries, self.gas_limit as u128)
 			.await;
 		Ok(())
@@ -192,8 +202,8 @@ where
 		bridge_transfer_id: BridgeTransferId<Self::Hash>,
 	) -> BridgeContractResult<()> {
 		let contract =
-			AtomicBridgeInitiator::new(self.initiator_address, self.rpc_provider.clone());
-		let call = contract.refundBridgeTransfer(bridge_transfer_id).await?;
+			AtomicBridgeInitiator::new(self.initiator_address, &self.rpc_provider.clone());
+		let call = contract.refundBridgeTransfer(FixedBytes(bridge_transfer_id.0));
 		send_tx(call, &self.send_tx_error_rules, self.num_tx_send_retries, self.gas_limit as u128)
 			.await;
 		Ok(())
@@ -204,11 +214,21 @@ where
 		bridge_transfer_id: BridgeTransferId<Self::Hash>,
 	) -> BridgeContractResult<Option<BridgeTransferDetails<Self::Hash, Self::Address>>> {
 		let contract =
-			AtomicBridgeInitiator::new(self.initiator_address, self.rpc_provider.clone());
-		let call = contract.getBridgeTransferDetails(bridge_transfer_id).await?;
-		send_tx(call, &self.send_tx_error_rules, self.num_tx_send_retries, self.gas_limit as u128)
-			.await;
+			AtomicBridgeInitiator::new(self.initiator_address, &self.rpc_provider.clone());
+		// look up the Details by Id
 		Ok(None)
+	}
+}
+
+fn vec_to_array(vec: Vec<u8>) -> Result<[u8; 32], &'static str> {
+	if vec.len() == 32 {
+		// Try to convert the Vec<u8> to [u8; 32]
+		match vec.try_into() {
+			Ok(array) => Ok(array),
+			Err(_) => Err("Failed to convert Vec<u8> to [u8; 32]"),
+		}
+	} else {
+		Err("Vec<u8> does not have exactly 32 elements")
 	}
 }
 
