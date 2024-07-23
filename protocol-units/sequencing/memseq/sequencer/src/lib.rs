d74e071459a9b0a76f217a9be00d3d8290c26678
Liam Monninger <l.mak.monninger@gmail.com>
2024-07-23 13:31:19 +0100

    fix: timing.

--- protocol-units/sequencing/memseq/sequencer/src/lib.rs
+++ protocol-units/sequencing/memseq/sequencer/src/lib.rs
@@ -42,7 +42,7 @@ impl Memseq<RocksdbMempool> {
 			path.to_str().ok_or(anyhow::anyhow!("PathBuf to str failed"))?,
 		)?;
 		let parent_block = Arc::new(RwLock::new(Id::default()));
-		Ok(Self::new(mempool, 1024, parent_block, 1000))
+		Ok(Self::new(mempool, 1024, parent_block, 100))
 	}
 
 	pub fn try_from_env_toml_file() -> Result<Self, anyhow::Error> {
