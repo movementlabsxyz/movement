79ee6322d06fde7cb8e3e3071729de47d4ba8996
Mikhail Zabaluev <mikhail.zabaluev@movementlabs.xyz>
2024-07-24 18:19:48 +0300

    feat(suzuka-full-node): env var for timing
    Conditionally enable synthetic span events in the tracing subscriber
    only if the SUZUKA_TIMING environment variable is set.

--- networks/suzuka/suzuka-full-node/src/main.rs
+++ networks/suzuka/suzuka-full-node/src/main.rs
@@ -1,21 +1,17 @@
 use maptos_dof_execution::v1::Executor;
 use suzuka_full_node::{manager::Manager, partial::SuzukaPartialNode};
 
-use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
+use tracing_subscriber::{filter::LevelFilter, fmt::format::FmtSpan, EnvFilter};
 
+use std::env;
 use std::process::ExitCode;
 
+const TIMING_ENV_VAR: &str = "SUZUKA_TIMING";
+
 fn main() -> Result<ExitCode, anyhow::Error> {
+	init_tracing_subscriber();
+
 	let runtime = tokio::runtime::Builder::new_multi_thread().enable_all().build()?;
-
-	// console_subscriber::init();
-
-	tracing_subscriber::fmt()
-		.with_env_filter(
-			EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
-		)
-		.with_span_events(FmtSpan::CLOSE)
-		.init();
 
 	if let Err(err) = runtime.block_on(run_suzuka()) {
 		tracing::error!("Suzuka node main task exit with an error : {err}",);
@@ -35,4 +31,16 @@ async fn run_suzuka() -> Result<ExitCode, anyhow::Error> {
 	manager.try_run().await?;
 
 	Ok(ExitCode::SUCCESS)
+}
+
+fn init_tracing_subscriber() {
+	// TODO: compose console_subscriber as a layer
+	let env_filter = EnvFilter::builder()
+		.with_default_directive(LevelFilter::INFO.into())
+		.from_env_lossy();
+	let mut subscriber = tracing_subscriber::fmt().with_env_filter(env_filter);
+	if env::var(TIMING_ENV_VAR).map_or(false, |v| v != "0") {
+		subscriber = subscriber.with_span_events(FmtSpan::CLOSE);
+	}
+	subscriber.init()
 }
