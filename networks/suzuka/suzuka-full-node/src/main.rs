f1da4f9ae9184cd4be43fc37f14516ec79a92deb
Mikhail Zabaluev <mikhail.zabaluev@movementlabs.xyz>
2024-07-24 10:54:04 +0300

    feat(suzuka-full-node): emit span events in log
    Enable span close events in the tracing-subscriber log.

--- networks/suzuka/suzuka-full-node/src/main.rs
+++ networks/suzuka/suzuka-full-node/src/main.rs
@@ -1,20 +1,20 @@
-use suzuka_full_node::{
-	manager::Manager,
-	partial::SuzukaPartialNode,
-};
 use maptos_dof_execution::v1::Executor;
+use suzuka_full_node::{manager::Manager, partial::SuzukaPartialNode};
+
+use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
+
 use std::process::ExitCode;
 
 fn main() -> Result<ExitCode, anyhow::Error> {
 	let runtime = tokio::runtime::Builder::new_multi_thread().enable_all().build()?;
 
 	// console_subscriber::init();
-	use tracing_subscriber::EnvFilter;
 
 	tracing_subscriber::fmt()
 		.with_env_filter(
 			EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
 		)
+		.with_span_events(FmtSpan::CLOSE)
 		.init();
 
 	if let Err(err) = runtime.block_on(run_suzuka()) {
@@ -27,10 +27,9 @@ fn main() -> Result<ExitCode, anyhow::Error> {
 }
 
 async fn run_suzuka() -> Result<ExitCode, anyhow::Error> {
-
 	// get the config file
 	let dot_movement = dot_movement::DotMovement::try_from_env()?;
-	let mut config_file = dot_movement.try_get_or_create_config_file().await?;
+	let config_file = dot_movement.try_get_or_create_config_file().await?;
 
 	let manager = Manager::<SuzukaPartialNode<Executor>>::new(config_file).await?;
 	manager.try_run().await?;
