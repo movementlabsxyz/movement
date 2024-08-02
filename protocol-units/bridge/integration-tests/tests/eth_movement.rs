a2121d4929fcd8e75a937321223bbe8db5b67d78
Richard Melkonian <r.v.melkonian@gmail.com>
2024-08-02 23:10:22 +0100

    feat: tighter assertions

--- protocol-units/bridge/integration-tests/tests/eth_movement.rs
+++ protocol-units/bridge/integration-tests/tests/eth_movement.rs
@@ -1,4 +1,8 @@
-use alloy::{node_bindings::Anvil, providers::Provider};
+use alloy::{
+	node_bindings::Anvil,
+	primitives::{address, Address},
+	providers::Provider,
+};
 use bridge_integration_tests::BridgeScaffold;
 use ethereum_bridge::AtomicBridgeInitiator;
 
@@ -15,6 +19,24 @@ async fn test_client_should_build_and_fetch_accounts() {
 
 	println!("Anvil running at `{}`", anvil.endpoint());
 
+	let expected_accounts = vec![
+		address!("f39fd6e51aad88f6f4ce6ab8827279cfffb92266"),
+		address!("70997970c51812dc3a010c7d01b50e0d17dc79c8"),
+		address!("3c44cdddb6a900fa2b585dd299e03d12fa4293bc"),
+		address!("90f79bf6eb2c4f870365e785982e1f101e93b906"),
+		address!("15d34aaf54267db7d7c367839aaf71a00a2c6a65"),
+		address!("9965507d1a55bcc2695c58ba16fb37d819b0a4dc"),
+		address!("976ea74026e726554db657fa54763abd0c3a0aa9"),
+		address!("14dc79964da2c08b23698b3d3cc7ca32193d9955"),
+		address!("23618e81e3f5cdf7f54c3d65f7fbc0abf5b21e8f"),
+		address!("a0ee7a142d267c1f36714e4a8f75612f20a79720"),
+	];
+
 	let provider = scaffold.eth_client.unwrap().rpc_provider().clone();
 	let accounts = provider.get_accounts().await.expect("Failed to get accounts");
+	assert_eq!(accounts.len(), expected_accounts.len());
+
+	for (account, expected) in accounts.iter().zip(expected_accounts.iter()) {
+		assert_eq!(account, expected);
+	}
 }
