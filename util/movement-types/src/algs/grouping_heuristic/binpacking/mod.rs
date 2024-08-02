353745132915a9305d57e114218ed8261fa6b3f2
Liam Monninger <l.mak.monninger@gmail.com>
2024-08-02 13:51:21 +0100

    fix: remove pub on block mods.

--- util/movement-types/src/algs/grouping_heuristic/binpacking/mod.rs
+++ util/movement-types/src/algs/grouping_heuristic/binpacking/mod.rs
@@ -3,114 +3,104 @@ pub use first_fit_decreasing::*;
 pub mod first_fit;
 pub use first_fit::*;
 
-use crate::algs::grouping_heuristic::{
-    ElementalOutcome,
-    ElementalFailure
-};
+use crate::algs::grouping_heuristic::{ElementalFailure, ElementalOutcome};
 
 pub trait BinpackingWeighted {
-    fn weight(&self) -> usize;
+	fn weight(&self) -> usize;
 }
 
-impl <T> BinpackingWeighted for ElementalFailure<T>
-where T: BinpackingWeighted
+impl<T> BinpackingWeighted for ElementalFailure<T>
+where
+	T: BinpackingWeighted,
 {
-
-    fn weight(&self) -> usize {
-        match self {
-            ElementalFailure::Instrumental(t) => t.weight(),
-            ElementalFailure::Terminal(t) => t.weight()
-        }
-    }
-
+	fn weight(&self) -> usize {
+		match self {
+			ElementalFailure::Instrumental(t) => t.weight(),
+			ElementalFailure::Terminal(t) => t.weight(),
+		}
+	}
 }
 
-impl <T> BinpackingWeighted for ElementalOutcome<T>
-where T: BinpackingWeighted
+impl<T> BinpackingWeighted for ElementalOutcome<T>
+where
+	T: BinpackingWeighted,
 {
-
-    fn weight(&self) -> usize {
-        match self {
-            ElementalOutcome::Success => 0,
-            ElementalOutcome::Apply(t) => t.weight(),
-            ElementalOutcome::Failure(failure) => failure.weight()
-        }
-    }
-
+	fn weight(&self) -> usize {
+		match self {
+			ElementalOutcome::Success => 0,
+			ElementalOutcome::Apply(t) => t.weight(),
+			ElementalOutcome::Failure(failure) => failure.weight(),
+		}
+	}
 }
-
 
 pub mod numeric {
 
-    use super::*;
+	use super::*;
 
-    impl BinpackingWeighted for usize {
-        fn weight(&self) -> usize {
-            *self
-        }
-    }
+	impl BinpackingWeighted for usize {
+		fn weight(&self) -> usize {
+			*self
+		}
+	}
 
-    impl BinpackingWeighted for i32 {
-        fn weight(&self) -> usize {
-            *self as usize
-        }
-    }
+	impl BinpackingWeighted for i32 {
+		fn weight(&self) -> usize {
+			*self as usize
+		}
+	}
 
-    impl BinpackingWeighted for i64 {
-        fn weight(&self) -> usize {
-            *self as usize
-        }
-    }
+	impl BinpackingWeighted for i64 {
+		fn weight(&self) -> usize {
+			*self as usize
+		}
+	}
 
-    impl BinpackingWeighted for f32 {
-        fn weight(&self) -> usize {
-            *self as usize
-        }
-    }
+	impl BinpackingWeighted for f32 {
+		fn weight(&self) -> usize {
+			*self as usize
+		}
+	}
 
-    impl BinpackingWeighted for f64 {
-        fn weight(&self) -> usize {
-            *self as usize
-        }
-    }
-
+	impl BinpackingWeighted for f64 {
+		fn weight(&self) -> usize {
+			*self as usize
+		}
+	}
 }
 
-pub mod block {
+mod block {
 
-    use super::*;
-    use crate::{Block, Transaction, Id};
+	use super::*;
+	use crate::{Block, Id, Transaction};
 
-    impl BinpackingWeighted for Id {
-        fn weight(&self) -> usize {
-            self.0.len()
-        }
-    }
+	impl BinpackingWeighted for Id {
+		fn weight(&self) -> usize {
+			self.0.len()
+		}
+	}
 
-    impl BinpackingWeighted for Transaction {
-        fn weight(&self) -> usize {
-            self.data.len() + self.id.weight()
-        }
-    }
+	impl BinpackingWeighted for Transaction {
+		fn weight(&self) -> usize {
+			self.data.len() + self.id.weight()
+		}
+	}
 
-    impl BinpackingWeighted for Block {
-        fn weight(&self) -> usize {
-        
-            // sum of the transactions
-            let mut weight = self.transactions.iter().map(|transaction| transaction.weight()).sum::<usize>();
+	impl BinpackingWeighted for Block {
+		fn weight(&self) -> usize {
+			// sum of the transactions
+			let mut weight =
+				self.transactions.iter().map(|transaction| transaction.weight()).sum::<usize>();
 
-            // id
-            weight += self.id.weight();
+			// id
+			weight += self.id.weight();
 
-            // parent
-            weight += self.parent.len();
+			// parent
+			weight += self.parent.len();
 
-            // for now metadata is negligible
+			// for now metadata is negligible
 
-            weight
-
-        }
-    }
-
-
+			weight
+		}
+	}
 }
--- util/movement-types/src/algs/grouping_heuristic/splitting.rs
+++ util/movement-types/src/algs/grouping_heuristic/splitting.rs
@@ -1,116 +1,121 @@
 use crate::algs::grouping_heuristic::{
-    GroupingHeuristic,
-    GroupingOutcome,
-    ElementalOutcome,
-    ElementalFailure
+	ElementalFailure, ElementalOutcome, GroupingHeuristic, GroupingOutcome,
 };
 use itertools::Itertools;
 
-pub trait Splitable where Self: Sized {
-    fn split(self, factor : usize) -> Vec<Self>;
+pub trait Splitable
+where
+	Self: Sized,
+{
+	fn split(self, factor: usize) -> Vec<Self>;
 }
 
-impl <T> Splitable for ElementalFailure<T>  where T : Splitable {
-
-    fn split(self, factor : usize) -> Vec<Self> {
-        
-        match self {
-            ElementalFailure::Instrumental(t) => t.split(factor).into_iter().map(ElementalFailure::Instrumental).collect(),
-            ElementalFailure::Terminal(t) => t.split(factor).into_iter().map(ElementalFailure::Terminal).collect()
-        }
-
-    }
-
+impl<T> Splitable for ElementalFailure<T>
+where
+	T: Splitable,
+{
+	fn split(self, factor: usize) -> Vec<Self> {
+		match self {
+			ElementalFailure::Instrumental(t) => {
+				t.split(factor).into_iter().map(ElementalFailure::Instrumental).collect()
+			}
+			ElementalFailure::Terminal(t) => {
+				t.split(factor).into_iter().map(ElementalFailure::Terminal).collect()
+			}
+		}
+	}
 }
-impl <T> Splitable for ElementalOutcome<T>  where T : Splitable {
-
-    fn split(self, factor : usize) -> Vec<Self> {
-        
-        match self {
-            ElementalOutcome::Success => vec![ElementalOutcome::Success],
-            ElementalOutcome::Apply(t) => t.split(factor).into_iter().map(ElementalOutcome::Apply).collect(),
-            ElementalOutcome::Failure(failure) => failure.split(factor).into_iter().map(ElementalOutcome::Failure).collect()
-        }
-
-    }
-
+impl<T> Splitable for ElementalOutcome<T>
+where
+	T: Splitable,
+{
+	fn split(self, factor: usize) -> Vec<Self> {
+		match self {
+			ElementalOutcome::Success => vec![ElementalOutcome::Success],
+			ElementalOutcome::Apply(t) => {
+				t.split(factor).into_iter().map(ElementalOutcome::Apply).collect()
+			}
+			ElementalOutcome::Failure(failure) => {
+				failure.split(factor).into_iter().map(ElementalOutcome::Failure).collect()
+			}
+		}
+	}
 }
 
 pub struct Splitting {
-    pub factor : usize
+	pub factor: usize,
 }
 
 impl Splitting {
-    pub fn new(factor : usize) -> Self {
-        Self { factor }
-    }
+	pub fn new(factor: usize) -> Self {
+		Self { factor }
+	}
 
-    pub fn boxed(factor : usize) -> Box<Self> {
-        Box::new(Self::new(factor))
-    }
-
+	pub fn boxed(factor: usize) -> Box<Self> {
+		Box::new(Self::new(factor))
+	}
 }
 
-impl <T> GroupingHeuristic<T> for Splitting 
-where T: Splitable {
-    
-    fn distribute(&mut self, distribution: Vec<GroupingOutcome<T>>) -> Result<Vec<GroupingOutcome<T>>, anyhow::Error> {
+impl<T> GroupingHeuristic<T> for Splitting
+where
+	T: Splitable,
+{
+	fn distribute(
+		&mut self,
+		distribution: Vec<GroupingOutcome<T>>,
+	) -> Result<Vec<GroupingOutcome<T>>, anyhow::Error> {
+		// reform each group by splitting each elemental outcome
+		let distribution = distribution
+			.into_iter()
+			.map(|outcome| {
+				outcome
+					.into_inner()
+					.into_iter()
+					.map(|outcome| outcome.split(self.factor))
+					.flatten()
+					.collect::<Vec<_>>()
+					.into()
+			})
+			.collect::<Vec<_>>();
 
-        // reform each group by splitting each elemental outcome
-        let distribution = distribution.into_iter().map(|outcome| 
-            outcome.into_inner().into_iter().map(|outcome| outcome.split(self.factor)).flatten().collect::<Vec<_>>().into()
-        ).collect::<Vec<_>>();
-
-        Ok(distribution)
-        
-    }
-
+		Ok(distribution)
+	}
 }
 
 impl<T> Splitable for Vec<T> {
-    fn split(self, factor: usize) -> Vec<Self> {
+	fn split(self, factor: usize) -> Vec<Self> {
+		let chunk_size = (self.len() as f64 / factor as f64).ceil() as usize;
 
-        let chunk_size = (self.len() as f64 / factor as f64).ceil() as usize;
-
-        // Calculate the chunk size based on the factor
-        self.into_iter()
-        .chunks(chunk_size)
-        .into_iter()
-        .map(|chunk| chunk.collect())
-        .collect() 
-    }
+		// Calculate the chunk size based on the factor
+		self.into_iter()
+			.chunks(chunk_size)
+			.into_iter()
+			.map(|chunk| chunk.collect())
+			.collect()
+	}
 }
 
-pub mod block {
+mod block {
 
-    use super::*;
-    use crate::Block;
+	use super::*;
+	use crate::Block;
 
-    impl Splitable for Block {
+	impl Splitable for Block {
+		fn split(self, factor: usize) -> Vec<Self> {
+			// unpack the transactions
+			let Block { metadata, transactions, parent, id: _ } = self;
 
-        fn split(self, factor : usize) -> Vec<Self> {
+			// split the vector of transactions
+			let split_transactions = transactions.split(factor);
 
-            // unpack the transactions
-            let Block {
-                metadata,
-                transactions, 
-                parent,
-                id: _
-            } = self;
+			// create a new block for each split transaction
+			let mut blocks = Vec::new();
+			for split in split_transactions {
+				let parent = Block::new(metadata.clone(), parent.clone(), split);
+				blocks.push(parent);
+			}
 
-            // split the vector of transactions
-            let split_transactions = transactions.split(factor);
-
-            // create a new block for each split transaction
-            let mut blocks = Vec::new();
-            for split in split_transactions {
-                let parent = Block::new(metadata.clone(), parent.clone(), split);
-                blocks.push(parent);
-            }
-
-            blocks
-        }
-
-    }
-
+			blocks
+		}
+	}
 }
