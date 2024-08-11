733d811d1c1947e1d9ef3c1b82e2fb47b759abcb
Another777 <74597507+HeroLeft@users.noreply.github.com>
2024-08-12 00:20:50 +0400

    Update flake.nix
    Using "let" to define common dependencies

--- flake.nix
+++ flake.nix
@@ -200,4 +200,24 @@
     );
 }
 
+let
+  commonDeps = [
+    pkgs.curl
+    pkgs.git
+  ];
+in
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default = nixpkgs.lib.mkShell {
+      buildInputs = commonDeps ++ [
+        # ... other dependencies ...
+      ];
+    };
+  };
+}
+
 
