43815a4968b65197b448d810cbd11864a53b6686
Richard Melkonian <r.v.melkonian@gmail.com>
2024-08-13 17:06:06 +0100

    feat: create SmartContractInitiator types for toplevel service crate

--- protocol-units/bridge/shared/src/lib.rs
+++ protocol-units/bridge/shared/src/lib.rs
@@ -2,6 +2,6 @@ pub mod blockchain_service;
 pub mod bridge_contracts;
 pub mod bridge_monitoring;
 pub mod bridge_service;
+pub mod initiator_contract;
 pub mod multiple_sources_of_truth;
 pub mod types;
-
