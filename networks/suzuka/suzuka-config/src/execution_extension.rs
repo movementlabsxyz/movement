c7954be5f80af214f7b69166318573f205da3807
Liam Monninger <l.mak.monninger@gmail.com>
2024-08-16 13:43:31 +0100

    chore: docs.

--- networks/suzuka/suzuka-config/src/execution_extension.rs
+++ networks/suzuka/suzuka-config/src/execution_extension.rs
@@ -1,15 +1,11 @@
 use godfig::env_default;
 use serde::{Deserialize, Serialize};
 
-/// The execution extension configuration.
-/// This covers Suzuka configurations that do not configure the Maptos executor, but do configure the way it is used.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Config {
-	/// The number of times to retry a block if it fails to execute.
 	#[serde(default = "default_block_retry_count")]
 	pub block_retry_count: u64,
 
-	/// The amount by which to increment the block timestamp if it fails to execute. (This is the most common reason for a block to fail to execute.)
 	#[serde(default = "default_block_retry_increment_microseconds")]
 	pub block_retry_increment_microseconds: u64,
 }
@@ -23,11 +19,11 @@ impl Default for Config {
 	}
 }
 
-env_default!(default_block_retry_count, "SUZUKA_BLOCK_RETRY_COUNT", u64, 10);
+env_default!(default_block_retry_count, "BLOCK_RETRY_COUNT", u64, 10);
 
 env_default!(
 	default_block_retry_increment_microseconds,
-	"SUZUKA_BLOCK_RETRY_INCREMENT_MICROSECONDS",
+	"BLOCK_RETRY_INCREMENT_MICROSECONDS",
 	u64,
 	5000
 );
