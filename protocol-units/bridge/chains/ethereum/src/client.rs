825da3f3a981c7507f4fb35b8faceb250588dd25
primata <primata@movementlabs.xyz>
2024-08-30 16:06:50 -0300

    add feature flag

--- protocol-units/bridge/chains/ethereum/src/client.rs
+++ protocol-units/bridge/chains/ethereum/src/client.rs
@@ -474,6 +474,7 @@ impl BridgeContractCounterparty for EthClient {
 	}
 }
 
+#[cfg(feature = "wrapping_to_test")]
 fn test_wrapping_to(a: &U256, b: u64) {
     assert_eq!(a.wrapping_to::<u64>(), b);
 }
