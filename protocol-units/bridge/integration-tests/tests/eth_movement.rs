c0002b1e6815807099f3259426d9849e7da9c5aa
Richard Melkonian <r.v.melkonian@gmail.com>
2024-09-04 18:53:23 +0100

    update: new test for index capture event

--- protocol-units/bridge/integration-tests/tests/eth_movement.rs
+++ protocol-units/bridge/integration-tests/tests/eth_movement.rs
@@ -103,7 +103,7 @@ async fn test_movement_client_should_successfully_call_lock_and_complete(
 		.movement_client_mut()
 		.expect("Failed to get MovmentClient")
 		.complete_bridge_transfer(
-			BridgeTransferId(bridge_transfer_id),
+			BridgeTransferId(args.bridge_transfer_id),
 			HashLockPreImage(b"secret".to_vec()),
 		)
 		.await
@@ -333,7 +333,37 @@ async fn test_harness_should_start_indexer() -> Result<(), anyhow::Error> {
 	let mut indexer_child = harness.start_indexer("127.0.0.1", 5432).await;
 
 	// Wait for the indexer process to complete
-	indexer_child.wait().await?;
+	tokio::time::sleep(tokio::time::Duration::from_secs(20)).await;
 	harness_child.kill().await.expect("Failed to kill the child process");
+	Ok(())
+}
+
+#[tokio::test]
+async fn test_indexer_should_capture_event_for_lock_call() -> Result<(), anyhow::Error> {
+	let _ = tracing_subscriber::fmt().with_max_level(tracing::Level::DEBUG).try_init();
+
+	let (mut harness, mut harness_child) = TestHarness::new_with_movement().await;
+	harness.start_indexer("127.0.0.1", 5432).await;
+	tokio::time::sleep(tokio::time::Duration::from_secs(20)).await;
+	{
+		let movement_client = harness.movement_client_mut().expect("Failed to get MovementClient");
+		let _ = movement_client.publish_for_test();
+	}
+
+	let args = harness.move_call_args();
+	harness
+		.movement_client_mut()
+		.expect("Failed to get MovmentClient")
+		.lock_bridge_transfer(
+			BridgeTransferId(args.bridge_transfer_id),
+			HashLock(args.hash_lock),
+			TimeLock(args.time_lock),
+			InitiatorAddress(args.initiator),
+			RecipientAddress(args.recipient),
+			Amount(AssetType::Moveth(args.amount)), // Eth
+		)
+		.await
+		.expect("Failed to complete bridge transfer");
+
 	Ok(())
 }
