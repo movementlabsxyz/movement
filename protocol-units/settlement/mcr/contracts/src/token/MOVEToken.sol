1e14939a1ab37261425fc00f510af4dc7b7d8a44
primata <primata@movementlabs.xyz>
2024-09-06 14:33:36 -0300

    rename token

--- protocol-units/settlement/mcr/contracts/src/token/MOVEToken.sol
+++ protocol-units/settlement/mcr/contracts/src/token/MOVEToken.sol
@@ -8,7 +8,7 @@ contract MOVEToken is MintableToken {
      * @dev Initialize the contract
      */
     function initialize() public initializer {
-        __MintableToken_init("Move Token", "MOVE");
+        __MintableToken_init("Movement", "MOVE");
         _mint(address(msg.sender), 10000000000 * 10 ** decimals());
     }
 }
