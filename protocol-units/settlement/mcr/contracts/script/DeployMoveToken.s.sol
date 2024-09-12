cb603d9f959c76e30c2bff0e9bbb80d179a57561
primata <primata@movementlabs.xyz>
2024-09-12 00:05:48 -0300

    feat: prep modify to include in CoreDeployment

--- protocol-units/settlement/mcr/contracts/script/DeployMoveToken.s.sol
+++ protocol-units/settlement/mcr/contracts/script/DeployMoveToken.s.sol
@@ -6,6 +6,7 @@ import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transpa
 import {SafeProxyFactory} from "@safe-smart-account/contracts/proxies/SafeProxyFactory.sol";
 import {Safe} from "@safe-smart-account/contracts/Safe.sol";
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+import {Vm} from "forge-std/Vm.sol";
 
 function string2Address(bytes memory str) returns (address addr) {
     bytes32 data = keccak256(str);
@@ -28,12 +29,12 @@ contract DeployMoveToken is Script {
     Safe public safe;
     TimelockController public timelock;
     address create3address = address(0x2Dfcc7415D89af828cbef005F0d072D8b3F23183);
+    address moveAdmin;
     bytes32 public salt = 0xc000000000000000000000002774b8b4881d594b03ff8a93f4cad69407c90350;
 
     function run() external {
         uint256 signer = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(signer);
-        MOVEToken moveImplementation = new MOVEToken();
 
         // forge script DeployMoveToken --fork-url https://eth-sepolia.api.onfinality.io/public
         safe = Safe(payable(address(0x00db70A9e12537495C359581b7b3Bc3a69379A00)));
@@ -52,24 +53,55 @@ contract DeployMoveToken is Script {
 
         timelock = new TimelockController(minDelay, proposers, executors, address(0x0));
 
-        // moveProxy = new TransparentUpgradeableProxy(
-        //     address(moveImplementation), address(timelock), abi.encodeWithSignature(moveSignature, address(safe))
-        // );
-
-        bytes memory bytecode = abi.encodePacked(
-            type(TransparentUpgradeableProxy).creationCode,
-            abi.encode(address(moveImplementation), address(timelock), abi.encodeWithSignature(moveSignature, address(safe)))
-        );
-
-        moveProxy = TransparentUpgradeableProxy(payable(create(create3address).deploy(salt, bytecode)));
+        
+        _deployMove();
+        
 
         console.log("Safe balance: ", MOVEToken(address(moveProxy)).balanceOf(address(safe)));
         console.log("Move Token decimals: ", MOVEToken(address(moveProxy)).decimals());
         console.log("Move Token supply: ", MOVEToken(address(moveProxy)).totalSupply());
         console.log("Timelock deployed at: ", address(timelock));
         console.log("Safe deployed at: ", address(safe));
-        console.log("Move Token deployed at: ", address(moveProxy));
-        console.log("implementation deployed at: ", address(moveImplementation));
         vm.stopBroadcast();
+    }
+
+    function _deployMove() internal {
+        console.log("MOVE: deploying");
+        MOVEToken moveImplementation = new MOVEToken();
+        // moveProxy = new TransparentUpgradeableProxy(
+        //     address(moveImplementation), address(timelock), abi.encodeWithSignature(moveSignature, address(safe))
+        // );
+        bytes memory bytecode = abi.encodePacked(
+            type(TransparentUpgradeableProxy).creationCode,
+            abi.encode(address(moveImplementation), address(timelock), abi.encodeWithSignature(moveSignature, address(safe)))
+        );
+        vm.recordLogs();
+        moveProxy = TransparentUpgradeableProxy(payable(create(create3address).deploy(salt, bytecode)));
+        Vm.Log[] memory logs = vm.getRecordedLogs();
+        console.log("MOVE deployment records:");
+        console.log("proxy", address(moveProxy));
+        console.log("implementation", address(moveImplementation));
+        // deployment.move = address(moveProxy);
+        // deployment.moveAdmin = _storeAdminDeployment();
+        
+        moveAdmin = logs[logs.length - 2].emitter;
+    }
+
+    function _upgradeMove() internal {
+        console.log("MOVE: upgrading");
+        MOVEToken newMoveImplementation = new MOVEToken();
+        timelock.schedule(
+            address(moveAdmin),
+            0,
+            abi.encodeWithSignature(
+                "upgradeAndCall(address,address,bytes)",
+                address(moveProxy),
+                address(newMoveImplementation),
+                abi.encodeWithSignature("initialize(address)", address(safe))
+            ),
+            bytes32(0),
+            bytes32(0),
+            block.timestamp + 1 days
+        );
     }
 }
--- protocol-units/settlement/mcr/contracts/src/token/MOVEToken.sol
+++ protocol-units/settlement/mcr/contracts/src/token/MOVEToken.sol
@@ -1,17 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 
-contract MOVEToken is ERC20Upgradeable {
+contract MOVEToken is ERC20PermitUpgradeable {
+
     /**
-     * @dev Initialize the contract
+     * @dev Disables potential implementation exploit
      */
-    function initialize(address multisig) public initializer {
+    constructor() {_disableInitializers();}
+
+    /**
+     * @dev Initializes the contract
+     * @param _owner The onwer of the initial supply
+     * @notice __ERC20_init params: name and symbol are set to "Movement" and "MOVE" respectively
+     * @notice __EIP712_init_unchained: name and version are set to "Movement" and "1" respectively
+     */
+    function initialize(address _owner) public initializer {
         __ERC20_init("Movement", "MOVE");
-        _mint(address(multisig), 10000000000 * 10 ** decimals());
+        __EIP712_init_unchained("Movement", "1");
+        _mint(address(_owner), 10000000000 * 10 ** decimals());
     }
 
+    /**
+     * @dev Returns the number of decimals
+     * @notice decimals is set to the Movement network standard decimals
+     */
     function decimals() public pure override returns (uint8) {
         return 8;
     }
