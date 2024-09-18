404e4dad5c48e1d688b247a119a51cae22e46d27
Liam Monninger <l.mak.monninger@gmail.com>
2024-09-18 10:53:35 +0100

    fix: add back monza aptos.

--- flake.nix
+++ flake.nix
@@ -84,6 +84,28 @@
           rustc = rust;
         };
 
+        monza-aptos = pkgs.stdenv.mkDerivation {
+            pname = "monza-aptos";
+            version = "branch-monza";
+
+            src = pkgs.fetchFromGitHub {
+                owner = "movementlabsxyz";
+                repo = "aptos-core";
+                rev = "06443b81f6b8b8742c4aa47eba9e315b5e6502ff";
+                sha256 = "sha256-iIYGbIh9yPtC6c22+KDi/LgDbxLEMhk4JJMGvweMJ1Q=";
+            };
+
+            installPhase = ''
+                cp -r . $out
+            '';
+
+            meta = with pkgs.lib; {
+                description = "Aptos core repository on the monza branch";
+                homepage = "https://github.com/movementlabsxyz/aptos-core";
+                license = licenses.asl20;
+            };
+        };
+
         aptos-faucet-service-common-args = {
           pname = "aptos-faucet-service";
           version = "2.0.1";
