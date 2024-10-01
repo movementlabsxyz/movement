66404ec75ed26a596191ae096b548959009336eb
Andy Golay <andygolay@gmail.com>
2024-10-01 01:42:47 -0400

    chore: remove unneeded comments

--- protocol-units/bridge/setup/src/lib.rs
+++ protocol-units/bridge/setup/src/lib.rs
@@ -24,18 +24,14 @@ impl Setup {
 	&self,
 	dot_movement: &DotMovement,
 	mut config: Config,
-	use_local: bool,  // Add this flag to control which setup to use
+	use_local: bool, 
 	) -> Result<(Config, AnvilInstance, Child), anyhow::Error> {
-	if use_local {
-		// First, run local setup if the flag is true
-		tracing::info!("Running local setup...");
-		let (config, anvil, child) = self.local.setup(dot_movement, config.clone()).await?;
-		Ok((config, anvil, child))
-	} else {
-		// Otherwise, run deploy setup
-		tracing::info!("Deploying contracts...");
-		let (config, anvil, child) = self.deploy.setup(dot_movement, config.clone()).await?;
-		Ok((config, anvil, child))
-	}
+		if use_local {
+			let (config, anvil, child) = self.local.setup(dot_movement, config.clone()).await?;
+			Ok((config, anvil, child))
+		} else {
+			let (config, anvil, child) = self.deploy.setup(dot_movement, config.clone()).await?;
+			Ok((config, anvil, child))
+		}
 	}
 }
