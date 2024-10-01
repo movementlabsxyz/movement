cdc6a5515ee596809bafbbc7514881b496c9a3a7
Liam Monninger <l.mak.monninger@gmail.com>
2024-09-30 21:32:28 -0700

    fix: remove v1 indexer API.

--- protocol-units/execution/opt-executor/src/executor/initialization.rs
+++ protocol-units/execution/opt-executor/src/executor/initialization.rs
@@ -29,6 +29,10 @@ impl Executor {
 	pub fn bootstrap(maptos_config: &Config) -> Result<Self, anyhow::Error> {
 		// set up the node config
 		let mut node_config = NodeConfig::default();
+
+		// rocksdb config
+		node_config.storage.rocksdb_configs.enable_storage_sharding = true;
+
 		// pruning config
 		node_config.storage.storage_pruner_config.ledger_pruner_config.prune_window =
 			maptos_config.chain.maptos_ledger_prune_window;
