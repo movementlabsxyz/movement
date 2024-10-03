794981aa2fd3e7463dfaf8f115bc9147f1b1b024
musitdev <philippe.delrieu@free.fr>
2024-10-03 11:27:53 +0200

    remove comment

--- protocol-units/bridge/service/src/chains/movement/client.rs
+++ protocol-units/bridge/service/src/chains/movement/client.rs
@@ -33,15 +33,6 @@ enum Call {
 	GetDetails,
 }
 
-// pub struct Config {
-// 	pub rpc_url: Option<String>,
-// 	pub ws_url: Option<String>,
-// 	pub chain_id: String,
-// 	pub signer_private_key: Arc<RwLock<LocalAccount>>,
-// 	pub initiator_contract: Option<MovementAddress>,
-// 	pub gas_limit: u64,
-// }
-
 #[allow(dead_code)]
 #[derive(Clone)]
 pub struct MovementClient {
