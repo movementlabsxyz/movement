461262aa281b708bcd91dce7969b5104161381ea
Andy Golay <andygolay@gmail.com>
2024-10-16 08:39:52 -0400

    fix: revert edit to event monitoring which caused E2E test to fail

--- protocol-units/bridge/service/src/chains/movement/event_monitoring.rs
+++ protocol-units/bridge/service/src/chains/movement/event_monitoring.rs
@@ -157,7 +157,6 @@ impl MovementMonitoring {
 			let config = config.clone();
 			async move {
 				let mvt_client = MovementClient::new(&config).await.unwrap();
-				println!("Movement client address: {:?}", mvt_client.native_address);
 				loop {
 					let mut init_event_list = match pool_initiator_contract(
 						mvt_client.native_address,
@@ -474,7 +473,6 @@ pub struct BridgeInitEventData {
 	pub time_lock: u64,
 	#[serde(deserialize_with = "deserialize_u64_from_string")]
 	pub amount: u64,
-	#[serde(deserialize_with = "deserialize_u8_from_string")]
 	pub state: u8,
 }
 
@@ -486,14 +484,6 @@ where
 	let hex_str: &str = Deserialize::deserialize(deserializer)?;
 	let hex_str = if hex_str.starts_with("0x") { &hex_str[2..] } else { &hex_str };
 	Vec::from_hex(hex_str).map_err(serde::de::Error::custom)
-}
-
-fn deserialize_u8_from_string<'de, D>(deserializer: D) -> Result<u8, D::Error>
-where
-	D: Deserializer<'de>,
-{
-	let s: String = Deserialize::deserialize(deserializer)?;
-	s.parse::<u8>().map_err(serde::de::Error::custom)
 }
 
 fn deserialize_u64_from_string<'de, D>(deserializer: D) -> Result<u64, D::Error>
