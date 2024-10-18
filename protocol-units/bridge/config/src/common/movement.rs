cb92bee8812166c66f2a36afe7d6e76cfda78920
Richard Melkonian <r.v.melkonian@gmail.com>
2024-10-18 12:07:31 +0100

    update: gRPC fields on config

--- protocol-units/bridge/config/src/common/movement.rs
+++ protocol-units/bridge/config/src/common/movement.rs
@@ -8,6 +8,8 @@ const DEFAULT_MVT_RPC_CONNECTION_PORT: u16 = 8080;
 const DEFAULT_MVT_FAUCET_CONNECTION_HOSTNAME: &str = "127.0.0.1";
 const DEFAULT_MVT_FAUCET_CONNECTION_PORT: u16 = 8081;
 const DEFAULT_REST_CONNECTION_HOSTNAME: &str = "127.0.0.1";
+const DEFAULT_GRPC_CONNECTION_HOSTNAME: &str = "127.0.0.1";
+const DEFAULT_GRPC_CONNECTION_PORT: u16 = 50051;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct MovementConfig {
@@ -33,10 +35,19 @@ pub struct MovementConfig {
 	#[serde(default = "default_mvt_init_network")]
 	pub mvt_init_network: String,
 
+	/// Endpoint for the REST service
 	#[serde(default = "default_rest_connection_hostname")]
 	pub rest_hostname: String,
 	#[serde(default = "default_rest_connection_port")]
 	pub rest_port: u32,
+
+	// gRPC service connection details
+	#[serde(default = "default_grpc_connection_protocol")]
+	pub grpc_protocol: String,
+	#[serde(default = "default_grpc_connection_hostname")]
+	pub grpc_hostname: String,
+	#[serde(default = "default_grpc_connection_port")]
+	pub grpc_port: u16,
 }
 
 // The default private key
@@ -46,6 +57,27 @@ pub fn default_movement_signer_key() -> Ed25519PrivateKey {
 		Err(_) => Ed25519PrivateKey::generate(&mut rand::thread_rng()),
 	}
 }
+
+env_default!(
+	default_grpc_connection_protocol,
+	"GRPC_CONNECTION_PROTOCOL",
+	String,
+	"http".to_string()
+);
+
+env_default!(
+	default_grpc_connection_hostname,
+	"GRPC_CONNECTION_HOSTNAME",
+	String,
+	DEFAULT_GRPC_CONNECTION_HOSTNAME.to_string()
+);
+
+env_default!(
+	default_grpc_connection_port,
+	"GRPC_CONNECTION_PORT",
+	u16,
+	DEFAULT_GRPC_CONNECTION_PORT
+);
 
 env_default!(
 	default_rest_connection_hostname,
@@ -143,6 +175,9 @@ impl MovementConfig {
 			mvt_init_network: default_mvt_init_network(),
 			rest_hostname: default_rest_connection_hostname(),
 			rest_port: default_rest_connection_port(),
+			grpc_protocol: default_grpc_connection_protocol(),
+			grpc_hostname: default_grpc_connection_hostname(),
+			grpc_port: default_grpc_connection_port(),
 		}
 	}
 }
@@ -161,6 +196,9 @@ impl Default for MovementConfig {
 			mvt_init_network: default_mvt_init_network(),
 			rest_hostname: default_rest_connection_hostname(),
 			rest_port: default_rest_connection_port(),
+			grpc_protocol: default_grpc_connection_protocol(),
+			grpc_hostname: default_grpc_connection_hostname(),
+			grpc_port: default_grpc_connection_port(),
 		}
 	}
 }
