c6b767777ea7ae0720568b79c0c167493ea94519
Richard Melkonian <r.v.melkonian@gmail.com>
2024-10-18 11:56:43 +0100

    update: fix up tests

--- protocol-units/bridge/integration-tests/tests/rest.rs
+++ protocol-units/bridge/integration-tests/tests/rest.rs
@@ -1,7 +1,11 @@
+use bridge_config::Config;
+use bridge_service::rest::BridgeRest;
+use poem::test::TestClient;
+use std::sync::Arc;
+use tracing_subscriber::EnvFilter;
+
 #[tokio::test]
 async fn test_rest_service_health_endpoint() {
-	use poem::test::TestClient;
-
 	tracing_subscriber::fmt()
 		.with_env_filter(
 			EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
@@ -9,20 +13,17 @@ async fn test_rest_service_health_endpoint() {
 		.init();
 
 	let mock_config = Config::default();
-
-	let rest_service = BridgeRest::new(mock_config.movement.clone()).unwrap();
+	let rest_service = Arc::new(BridgeRest::new(&mock_config.movement).unwrap());
+	let rest_service_for_task = Arc::clone(&rest_service);
 
 	let rest_service_future = tokio::spawn(async move {
-		rest_service.run_service().await.unwrap();
+		rest_service_for_task.run_service().await.unwrap();
 	});
-
-	sleep(Duration::from_millis(500)).await;
 
 	let client = TestClient::new(rest_service.create_routes());
 
 	let response = client.get("/health").send().await;
-	assert!(response.status().is_success(), "Health endpoint failed");
-	assert_eq!(response.text().await.unwrap(), "OK");
+	response.assert_status_is_ok();
 
 	rest_service_future.abort();
 }
