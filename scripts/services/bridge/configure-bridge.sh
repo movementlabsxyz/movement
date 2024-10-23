9dd5234e281f979daea0db34aeff6c28aa223613
Richard Melkonian <r.v.melkonian@gmail.com>
2024-10-23 14:57:42 +0100

    chore: remove unneeded

--- scripts/services/bridge/configure-bridge.sh
+++ scripts/services/bridge/configure-bridge.sh
@@ -4,16 +4,13 @@
 MOVEMENT_DIR="./.movement"
 CONFIG_FILE="$MOVEMENT_DIR/config.yaml"
 
-# The new account value you want to set
 NEW_ACCOUNT="0xA550C18"
 
-# Check if the .movement directory exists
 if [ ! -d "$MOVEMENT_DIR" ]; then
   echo "Error: Directory $MOVEMENT_DIR not found."
   exit 1
 fi
 
-# Check if the config.yaml file exists
 if [ ! -f "$CONFIG_FILE" ]; then
   echo "Error: File $CONFIG_FILE not found."
   exit 1
@@ -29,4 +26,3 @@ else
 fi
 
 echo "Account field updated with value: ${NEW_ACCOUNT}"
-
