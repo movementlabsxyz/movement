1eff1b57e9bbd8fe2fa822985423c9d4fe4f0ddf
Liam Monninger <l.mak.monninger@gmail.com>
2024-10-25 11:06:30 -0700

    fix: accept.

--- protocol-units/execution/opt-executor/src/executor/execution.rs
+++ protocol-units/execution/opt-executor/src/executor/execution.rs
@@ -16,10 +16,9 @@ use aptos_types::{
 	validator_verifier::{ValidatorConsensusInfo, ValidatorVerifier},
 };
 use movement_types::block::{BlockCommitment, Commitment, Id};
-use tracing::{info, warn};
+use tracing::{debug, info, warn};
 
 impl Executor {
-	/// Executes a block and commits it to the storage layer.
 	pub async fn execute_block(
 		&self,
 		block: ExecutableBlock,
@@ -65,9 +64,10 @@ impl Executor {
 		})
 		.await??;
 
-		info!("Block execution compute the following state: {:?}", state_compute);
+		warn!("Block execution compute the following state: {:?}", state_compute);
+
 		let version = state_compute.version();
-		info!("Block execution computed the following version: {:?}", version);
+		debug!("Block execution computed the following version: {:?}", version);
 		let (epoch, round) = (block_metadata.epoch(), block_metadata.round());
 
 		let ledger_info_with_sigs = self.ledger_info_with_sigs(
@@ -141,8 +141,9 @@ impl Executor {
 
 	pub async fn rollover_genesis(&self, timestamp: u64) -> Result<(), anyhow::Error> {
 		let (epoch, round) = self.get_next_epoch_and_round()?;
-		let block_id = HashValue::from_hex(self.config().chain.genesis_block_hash_hex.as_str())?;
+		let block_id = HashValue::random();
 
+		// genesis timestamp should always be 0
 		let genesis_timestamp = self.get_last_state_timestamp_micros()?;
 		info!(
 			"Rollover genesis: epoch: {}, round: {}, block_id: {}, genesis timestamp {}",
@@ -365,7 +366,7 @@ mod tests {
 					Transaction::UserTransaction(user_account_creation_tx),
 					Transaction::UserTransaction(mint_tx),
 				]));
-			info!("Number of transactions: {}", transactions.num_transactions());
+			debug!("Number of transactions: {}", transactions.num_transactions());
 			let block = ExecutableBlock::new(block_id.clone(), transactions);
 			let block_commitment = executor.execute_block(block).await?;
 
--- protocol-units/execution/opt-executor/src/executor/initialization.rs
+++ protocol-units/execution/opt-executor/src/executor/initialization.rs
@@ -8,6 +8,7 @@ use aptos_crypto::PrivateKey;
 use aptos_executor::block_executor::BlockExecutor;
 use aptos_mempool::MempoolClientRequest;
 use aptos_types::transaction::SignedTransaction;
+use dot_movement::DotMovement;
 use futures::FutureExt;
 use maptos_execution_util::config::Config;
 
@@ -27,6 +28,10 @@ const EXECUTOR_CHANNEL_SIZE: usize = 2_usize.pow(16);
 
 impl Executor {
 	pub fn bootstrap(maptos_config: &Config) -> Result<Self, anyhow::Error> {
+		// get dot movement
+		// todo: this is a slight anti-pattern, but it's fine for now
+		let dot_movement = DotMovement::try_from_env()?;
+
 		// set up the node config
 		let mut node_config = NodeConfig::default();
 
@@ -74,7 +79,7 @@ impl Executor {
 
 		// indexer table info config
 		node_config.indexer_table_info.enabled = true;
-		node_config.storage.dir = "./.movement/maptos-storage".to_string().into();
+		node_config.storage.dir = dot_movement.get_path().join("maptos-storage");
 		node_config.storage.set_data_dir(node_config.storage.dir.clone());
 
 		let (db, signer) = bootstrap::maybe_bootstrap_empty_db(
@@ -132,8 +137,8 @@ impl Executor {
 			&node_config,
 			Arc::clone(&self.transactions_in_flight),
 			maptos_config.load_shedding.max_transactions_in_flight,
-			self.config.mempool.sequence_number_ttl_ms,
-			self.config.mempool.gc_slot_duration_ms,
+			self.config().mempool.sequence_number_ttl_ms,
+			self.config().mempool.gc_slot_duration_ms,
 		);
 
 		let cx = Context::new(
