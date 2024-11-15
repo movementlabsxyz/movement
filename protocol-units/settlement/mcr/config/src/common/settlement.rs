174938c6cc26324c7bfe121b24b4d6e081f99a7e
Liam Monninger <l.mak.monninger@gmail.com>
2024-11-15 12:29:02 +0100

    fix: super blocks.

--- protocol-units/settlement/mcr/config/src/common/settlement.rs
+++ protocol-units/settlement/mcr/config/src/common/settlement.rs
@@ -13,6 +13,8 @@ pub struct Config {
 	pub signer_private_key: String,
 	#[serde(default = "default_mcr_contract_address")]
 	pub mcr_contract_address: String,
+	#[serde(default = "default_settlement_super_block_size")]
+	pub settlement_super_block_size: u64,
 	#[serde(default = "default_settlement_admin_mode")]
 	pub settlement_admin_mode: bool,
 }
@@ -32,6 +34,8 @@ env_default!(
 
 env_default!(default_settlement_admin_mode, "MCR_SETTLEMENT_ADMIN_MODE", bool, false);
 
+env_default!(default_settlement_super_block_size, "MCR_SETTLEMENT_SUPER_BLOCK_SIZE", u64, 1);
+
 pub fn default_should_settle() -> bool {
 	env::var("ETH_SIGNER_PRIVATE_KEY").is_ok()
 }
@@ -43,6 +47,7 @@ impl Default for Config {
 			signer_private_key: default_signer_private_key(),
 			mcr_contract_address: default_mcr_contract_address(),
 			settlement_admin_mode: default_settlement_admin_mode(),
+			settlement_super_block_size: default_settlement_super_block_size(),
 		}
 	}
 }
