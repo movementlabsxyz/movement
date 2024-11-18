22ab9e77c9e5ebe21778cec6d2cd90643baf591f
primata <primata@movementlabs.xyz>
2024-11-18 13:45:41 +0100

    add move to simple

---
+++ protocol-units/bridge/contracts/src/INativeBridge.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+interface INativeBridge {
+    // Event emitted when a new native bridge transfer is created
+    event BridgeTransferInitiated(
+        bytes32 indexed bridgeTransferId,
+        address indexed originator,
+        bytes32 indexed recipient,
+        uint256 amount,
+        uint256 nonce
+    );
+    // Event emitted when a BridgeTransfer is completed (withdrawn)
+    event BridgeTransferCompleted(
+        bytes32 indexed bridgeTransferId,
+        bytes32 indexed originator,
+        address indexed recipient,
+        uint256 amount,
+        uint256 nonce
+    );
+
+    error ZeroAmount();
+    error MOVETransferFailed();
+    error ZeroAddress();
+    error InvalidLenghts();
+
+    /**
+     * @dev Creates a new bridge
+     * @param recipient The address on the other chain to which to transfer funds
+     * @param amount The amount of MOVE to send
+     * @return bridgeTransferId A unique id representing this BridgeTransfer
+     *
+     */
+    function initiateBridge(bytes32 recipient, uint256 amount)
+        external
+        returns (bytes32 bridgeTransferId);
+
+    /**
+     * @dev Completes the bridging Counterparty
+     * @param bridgeTransferId Unique identifier for the BridgeTransfer
+     * @param originator The address on the other chain that originated the transfer of funds
+     * @param recipient The address on this chain to which to transfer funds
+     * @param amount The amount to transfer
+     * @param nonce The seed nonce to generate the bridgeTransferId
+     *
+     */
+    function completeBridge(
+        bytes32 bridgeTransferId,
+        bytes32 originator,
+        address recipient,
+        uint256 amount,
+        uint256 nonce
+        ) external;
+}
---
+++ protocol-units/bridge/contracts/src/NativeBridge.sol
@@ -0,0 +1,115 @@
+
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import {RateLimiter} from "./RateLimiter.sol";
+import {INativeBridge} from "./INativeBridge.sol";
+
+contract NativeBridgeMOVE is RateLimiter, AccessControlUpgradeable, INativeBridgeMOVE {
+   
+   struct OutgoingBridgeTransfer {
+      address originator,
+      bytes32 recipient,
+      uint256 amount,
+      uint256 nonce
+   }
+   mapping(bytes32 bridgeTransferId => OutgoingBridgeTransfer) outgoingBridgeTransfers;
+
+   struct IncomingBridgeTransfer {
+      bytes32 originator,
+      address recipient,
+      uint256 amount,
+      uint256 nonce
+   }
+   mapping(bytes32 bridgeTransferId => IncomingBridgeTransfer) incomingBridgeTransfers;
+
+   bytes32 public constant RELAYER_ROLE = keccak256(abi.encodePacked("RELAYER_ROLE"));
+   uint256 private _nonce;
+
+    // Prevents initialization of implementation contract exploits
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address _admin, address _relayer, address _maintainer) public initializer {
+        require(_admin != address(0) && _relayer != address(0), ZeroAddress());
+        _grantRole(_admin, DEFAULT_ADMIN_ROLE);
+        _grantRole(_relayer, RELAYER_ROLE);
+        _grantRole(_maintainer, RELAYER_ROLE);
+    }
+
+ function initiateBridge(uint256 recipient, uint256 amount) external returns (bytes32 bridgeTransferId)
+    {
+        // Ensure there is a valid amount
+        require(amount > 0, ZeroAmount());
+        _l1l2RateLimit(amount);
+        address originator = msg.sender;
+
+        // Transfer the MOVE tokens from the user to the contract
+        if (moveToken.transferFrom(originator, address(this), amount)) revert MOVETransferFailed();
+
+        // Generate a unique nonce to prevent replay attacks, and generate a transfer ID
+        bridgeTransferId =
+            keccak256(abi.encodePacked(originator, recipient, amount, ++_nonce));
+
+        bridgeTransfers[bridgeTransferId] = OutgoingBridgeTransfer({
+            originator,
+            recipient,
+            amount,
+            _nonce
+        });
+
+        emit BridgeTransferInitiated(bridgeTransferId, originator, recipient, amount, _nonce);
+        return bridgeTransferId;
+    }
+
+function completeBridge(
+        bytes32 bridgeTransferId,
+        bytes32 originator,
+        address recipient,
+        uint256 amount,
+        uint256 nonce
+        ) external onlyRole(RELAYER_ROLE) {
+         _completeBridge(bridgeTransferId, originator, recipient, amount, nonce);
+         
+    }
+function batchCompleteBridge(
+   bytes32[] bridgeTransferIds,
+   bytes32[] originators,
+   address[] recipients,
+   uint256[] amounts,
+   uint256[] nonces
+) external onlyRole(RELAYER_ROLE) {
+   uint256 length = bridgeTransferIds.length;
+   require(originators.length == length && recipients.length == length && amounts.length == length && nonces.length == length, InvalidLenghts());
+   for (uint256 i; i < length; i++) {
+      _completeBridge(
+         bridgeTransferIds[i],
+         originators[i],
+         recipients[i],
+         amounts[i],
+         nonces[i]
+      )
+   }
+}
+
+function _completeBridge(
+   bytes32 bridgeTransferId,
+   bytes32 originator,
+   address recipient,
+   uint256 amount,
+   uint256 nonce
+) internal {
+   _l2l1RateLimit(amount);
+   require(bridgeTransferId == keccak256(abi.encodePacked(originator, recipient, amount, nonce)), InvalidBridgeTransferId());
+   require(bridgeTransfers[bridgeTranserId].amount == 0);
+   incomingBridgeTransfers[bridgeTranserId] = ({
+   originator,
+   recipient,
+   amount,
+   nonce
+   });
+
+   if (moveToken.transfer(recipient, amount)) revert MOVETransferFailed();
+
+   emit BridgeTransferCompleted(bridgeTransferId, originator, recipient, amount, nonce);
+   }
+}
