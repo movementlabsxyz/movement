d929f6ba44e1505d1d7fafc4fc95c2c1776d13f3
Mikhail Zabaluev <mikhail.zabaluev@movementlabs.xyz>
2024-11-19 21:02:51 +0200

    feat(da-light-node-grpc): expose versioning
    As minimal future-proofness, include the generated proto code under the
    v1beta1 module, corresponding to the versioned Protobuf package name.
    The names from the module are re-exported at the top level as will
    be the latest versioned package.

--- protocol-units/da/grpc/light-node/src/lib.rs
+++ protocol-units/da/grpc/light-node/src/lib.rs
@@ -1,3 +1,8 @@
-tonic::include_proto!("movementlabs.protocol_units.da.m1.light_node.v1beta1"); // The string specified here
-pub const FILE_DESCRIPTOR_SET: &[u8] =
-	tonic::include_file_descriptor_set!("movement-da-light-node-grpc-descriptor");
+pub mod v1beta1 {
+	tonic::include_proto!("movementlabs.protocol_units.da.light_node.v1beta1"); // The string specified here
+	pub const FILE_DESCRIPTOR_SET: &[u8] =
+		tonic::include_file_descriptor_set!("movement-da-light-node-grpc-descriptor");
+}
+
+// Re-export the latest version at the crate root
+pub use v1beta1::*;
