3b926693fa7b318399cdcf560fb55fc859463bb0
primata <primata@movementlabs.xyz>
2024-11-25 16:33:01 +0100

    rate limit comments

--- protocol-units/bridge/contracts/src/RateLimiter.sol
+++ protocol-units/bridge/contracts/src/RateLimiter.sol
@@ -7,8 +7,11 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract RateLimiter is AccessControlUpgradeable {
     bytes32 public constant ATOMIC_BRIDGE = keccak256("ATOMIC_BRIDGE");
 
+    // Rate limit budget for outbound transfers
     mapping(uint256 day => uint256 amount) public outboundRateLimitBudget;
+    // Rate limit budget for inbound transfers
     mapping(uint256 day => uint256 amount) public inboundRateLimitBudget;
+    // Address of the insurance fund
     address public insuranceFund;
     IERC20 public moveToken;
 
@@ -33,12 +36,18 @@ contract RateLimiter is AccessControlUpgradeable {
         insuranceFund = _insuranceFund;
     }
 
+    // Rate limit the amount of MOVE tokens that can be transferred to the Atomic Bridge
+    // The rate limit is set to 25% of the total MOVE tokens in the insurance fund
+    // Only instances of the AtomicBridge contract can call this function
     function rateLimitOutbound(uint256 amount) external onlyRole(ATOMIC_BRIDGE) {
         uint256 day = block.timestamp / 1 days;
         outboundRateLimitBudget[day] += amount;
         require(outboundRateLimitBudget[day] < moveToken.balanceOf(insuranceFund) / 4, OutboundRateLimitExceeded());
     }
 
+    // Rate limit the amount of MOVE tokens that can be transferred from the Atomic Bridge
+    // The rate limit is set to 25% of the total MOVE tokens in the insurance fund
+    // Only instances of the AtomicBridge contract can call this function
     function rateLimitInbound(uint256 amount) external onlyRole(ATOMIC_BRIDGE) {
         uint256 day = block.timestamp / 1 days;
         inboundRateLimitBudget[day] += amount;
