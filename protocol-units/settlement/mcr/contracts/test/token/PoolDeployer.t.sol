f412e6745c27b7eeccb206526b43007e1bfa214e
primata <primata@movementlabs.xyz>
2024-12-08 18:45:34 -0800

    add deployer test

--- protocol-units/settlement/mcr/contracts/test/token/PoolDeployer.t.sol
+++ protocol-units/settlement/mcr/contracts/test/token/PoolDeployer.t.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.13;
 
-import "forge-std/Script.sol";
+import "forge-std/Test.sol";
 import "v2-core/interfaces/IUniswapV2Factory.sol";
 import "v2-core/interfaces/IUniswapV2Pair.sol";
 import {IERC20} from "v2-core/interfaces/IERC20.sol";
@@ -8,28 +8,26 @@ import "v2-periphery/interfaces/IUniswapV2Router02.sol";
 import "forge-std/console.sol";
 
 
-contract PoolDeployer is Script {
-    uint256 privateKey = vm.envUint("PRIVATE_KEY");
+contract PoolDeployer is Test {
+
     IUniswapV2Router02 public router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
     IUniswapV2Factory public factory = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
     IUniswapV2Pair public pair;
 
     address moveAddress = 0x3073f7aAA4DB83f95e9FFf17424F71D4751a3073;
     address wethAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address deployer = 0xB2105464215716e1445367BEA5668F581eF7d063;
 
     uint256 public moveDecimals = 8;
     uint256 public ethDecimals = 18;
     uint256 public moveTotalSupply = 10* 10**9 * 10**moveDecimals;
     uint256 public moveAndEthDepositValue = 250_000;
 
-    uint256 public targetValuation = 1_500_000_000;
+    uint256 public targetValuation = 500_000_000;
     uint256 public currentEthPrice = 4000;
 
-    address public deployer = 0xB2105464215716e1445367BEA5668F581eF7d063;
-    address public receiver;
-
-    function run() external {
-        vm.startBroadcast(privateKey);
+    function testDeploy() external {
+        vm.startPrank(deployer);
 
         uint256 ethAmount = (moveAndEthDepositValue * 1e18) / currentEthPrice;
         uint256 movePriceInEth = targetValuation * 1e18 / moveTotalSupply;
@@ -37,12 +35,13 @@ contract PoolDeployer is Script {
         console.log("moveAmount: ", moveAmount);
         console.log("ethAmount: ", ethAmount);
 
-        vm.assertEq(vm.addr(privateKey), deployer);
+        deal(moveAddress, deployer, moveAmount);
+        deal(wethAddress, deployer, ethAmount);
 
         IERC20(moveAddress).approve(address(router), moveAmount);
         IERC20(wethAddress).approve(address(router), ethAmount);
         pair = IUniswapV2Pair(factory.createPair(moveAddress, wethAddress));
-        router.addLiquidity(moveAddress, wethAddress, moveAmount, ethAmount, moveAmount, ethAmount, vm.addr(privateKey), block.timestamp + 1000);
-        vm.stopBroadcast();
+        router.addLiquidity(moveAddress, wethAddress, moveAmount, ethAmount, moveAmount, ethAmount, deployer, block.timestamp + 1000);
+        vm.stopPrank();
     }
 }
