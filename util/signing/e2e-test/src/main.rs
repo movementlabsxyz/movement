155ef99d9ae7a7da6dad5f4ea464b439443c4ca1
musitdev <philippe.delrieu@free.fr>
2025-01-02 15:11:10 +0100

    define on test for both setup

--- util/signing/e2e-test/src/main.rs
+++ util/signing/e2e-test/src/main.rs
@@ -1,14 +1,8 @@
 // Run the e2e signing Test.
 // e2e signing test are run using real node: 'ex Anvil and Suzuka node).
-
-// Use the local signer to sign Eth Tx. Can be run in the CI.
+// Depending on the executed setup it doesn't use the same key/provided..
 #[tokio::test]
-async fn e2e_eth_signing_local() -> Result<(), anyhow::Error> {
-	todo!()
-}
-
-// Use the AWS KMS signer to sign Eth Tx. AWS auth env var must be set to run the test.
-#[tokio::test]
-async fn e2e_eth_signing_awskms() -> Result<(), anyhow::Error> {
+async fn e2e_eth_signing() -> Result<(), anyhow::Error> {
+	//use the ETH_TEST_KEY1 name to get the key from the Key Manager.
 	todo!()
 }
