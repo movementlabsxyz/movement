a66b82abd3c7be43b7acf710dd811c3c73fb01c3
Richard Melkonian <r.v.melkonian@gmail.com>
2025-01-20 17:44:01 +0000

    fix: config step

--- networks/movement/movement-client/src/bin/e2e/ggp_non_native_token.rs
+++ networks/movement/movement-client/src/bin/e2e/ggp_non_native_token.rs
@@ -86,6 +86,10 @@ async fn main() -> Result<(), anyhow::Error> {
 	);
 	let crate_dir_clone = crate_dir.clone();
 
+	let target_dir = PathBuf::from(crate_dir).join("src").join("move-modules");
+	let target_dir_clone = target_dir.clone();
+
+	println!("target_dir: {:?}", target_dir);
 	println!("Node URL: {:?}", NODE_URL.as_str());
 	println!("Faucet URL: {:?}", FAUCET_URL.as_str());
 
@@ -104,6 +108,7 @@ async fn main() -> Result<(), anyhow::Error> {
 			"--private-key",
 			PRIVATE_KEY,
 		])
+		.current_dir(target_di)
 		.status()
 		.await
 		.expect("Failed to execute `movement init` command");
