f47ce675904cbaa1d410cfd5f0e4148fb4026449
Liam Monninger <l.mak.monninger@gmail.com>
2025-01-21 23:22:58 -0800

    fix: add key roation subcommand.

--- networks/movement/movement-full-node/src/admin/mod.rs
+++ networks/movement/movement-full-node/src/admin/mod.rs
@@ -1,16 +1,19 @@
 pub mod force_commitment;
+pub mod rotate_key;
 use clap::Subcommand;
 
 #[derive(Subcommand, Debug)]
 #[clap(rename_all = "kebab-case", about = "Commands for syncing")]
 pub enum Admin {
 	ForceCommitment(force_commitment::ForceCommitment),
+	RotateKey(rotate_key::RotateKey),
 }
 
 impl Admin {
 	pub async fn execute(&self) -> Result<(), anyhow::Error> {
 		match self {
 			Admin::ForceCommitment(force_commitment) => force_commitment.execute().await,
+			Admin::RotateKey(rotate_key) => rotate_key.execute().await,
 		}
 	}
 }
