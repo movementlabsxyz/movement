899baddf8acf637173045b337f370e56545b437a
primata <primata@movementlabs.xyz>
2025-01-24 05:25:16 +0400

    low-level error return

--- networks/movement/movement-client/src/bin/e2e/lock_mint.rs
+++ networks/movement/movement-client/src/bin/e2e/lock_mint.rs
@@ -2,11 +2,20 @@
 use anyhow::{Chain, Context};
 use movement_client::crypto::ValidCryptoMaterialStringExt;
 
-use aptos_sdk::{crypto::multi_ed25519::MultiEd25519PublicKey, crypto::ed25519::ed25519_keys::Ed25519PublicKey, move_types::{
-	identifier::Identifier, language_storage::{ModuleId, StructTag, TypeTag},
-}, rest_client::Account};
 use aptos_sdk::types::{
-	account_address::AccountAddress, chain_id::ChainId, transaction::{EntryFunction, TransactionArgument, Script}, LocalAccount, AccountKey
+	account_address::AccountAddress,
+	chain_id::ChainId,
+	transaction::{EntryFunction, Script, TransactionArgument},
+	AccountKey, LocalAccount,
+};
+use aptos_sdk::{
+	crypto::ed25519::ed25519_keys::Ed25519PublicKey,
+	crypto::multi_ed25519::MultiEd25519PublicKey,
+	move_types::{
+		identifier::Identifier,
+		language_storage::{ModuleId, StructTag, TypeTag},
+	},
+	rest_client::Account,
 };
 use aptos_sdk::{
 	rest_client::{
@@ -18,19 +27,22 @@ use aptos_sdk::{
 	},
 	transaction_builder::TransactionBuilder,
 };
-use aptos_types::{transaction::TransactionPayload, account_config::aptos_test_root_address, test_helpers::transaction_test_helpers};
+use aptos_types::{
+	account_config::aptos_test_root_address, test_helpers::transaction_test_helpers,
+	transaction::TransactionPayload,
+};
 use movement_client::{
 	coin_client::CoinClient,
 	rest_client::{Client, FaucetClient},
 };
 use once_cell::sync::Lazy;
 use rayon::vec;
+use std::process::Command;
 use std::str::FromStr;
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::{env, fs};
 use tracing;
 use url::Url;
-use std::process::Command;
-use std::{env, fs};
 
 static SUZUKA_CONFIG: Lazy<movement_config::Config> = Lazy::new(|| {
 	let dot_movement = dot_movement::DotMovement::try_from_env().unwrap();
@@ -79,10 +91,12 @@ async fn main() -> Result<(), anyhow::Error> {
 	let faucet_client = FaucetClient::new(FAUCET_URL.clone(), NODE_URL.clone());
 	let coin_client = CoinClient::new(&rest_client);
 	let dead_address = AccountAddress::from_str(
-		"000000000000000000000000000000000000000000000000000000000000dead")?;
+		"000000000000000000000000000000000000000000000000000000000000dead",
+	)?;
 	let associate_address = AccountAddress::from_str(
-		"0x000000000000000000000000000000000000000000000000000000000a550c18")?;
-	
+		"0x000000000000000000000000000000000000000000000000000000000a550c18",
+	)?;
+
 	let chain_id = rest_client
 		.get_index()
 		.await
@@ -100,31 +114,36 @@ async fn main() -> Result<(), anyhow::Error> {
 			.as_str(),
 		0,
 	)?;
-	println!("resource account keypairs: {:?}, {:?}", core_resources_account.private_key(), core_resources_account.public_key());
+	println!(
+		"resource account keypairs: {:?}, {:?}",
+		core_resources_account.private_key(),
+		core_resources_account.public_key()
+	);
 	println!("Core Resources Account address: {}", core_resources_account.address());
 
 	tracing::info!("Created core resources account");
 	// core_resources_account is already funded with u64 max value
 	// Create dead account
-	let create_dead_transaction = transaction_test_helpers::get_test_signed_transaction_with_chain_id(
-        associate_address,
-        core_resources_account.sequence_number(),
-        &core_resources_account.private_key(),
-        core_resources_account.public_key().clone(),
-        Some(TransactionPayload::EntryFunction(EntryFunction::new(
-			ModuleId::new(
-				AccountAddress::from_hex_literal("0x1")?,
-				Identifier::new("aptos_account")?,
-			),
-			Identifier::new("create_account")?,
-			vec![],
-			vec![bcs::to_bytes(&dead_address)?],
-		))),
-		SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() + 60,
-		100,
-		None,
-		ChainId::new(chain_id),
-    );
+	let create_dead_transaction =
+		transaction_test_helpers::get_test_signed_transaction_with_chain_id(
+			associate_address,
+			core_resources_account.sequence_number(),
+			&core_resources_account.private_key(),
+			core_resources_account.public_key().clone(),
+			Some(TransactionPayload::EntryFunction(EntryFunction::new(
+				ModuleId::new(
+					AccountAddress::from_hex_literal("0x1")?,
+					Identifier::new("aptos_account")?,
+				),
+				Identifier::new("create_account")?,
+				vec![],
+				vec![bcs::to_bytes(&dead_address)?],
+			))),
+			SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() + 60,
+			100,
+			None,
+			ChainId::new(chain_id),
+		);
 
 	rest_client
 		.submit_and_wait(&create_dead_transaction)
@@ -133,7 +152,8 @@ async fn main() -> Result<(), anyhow::Error> {
 
 	core_resources_account.increment_sequence_number();
 
-		let create_signer_transaction = transaction_test_helpers::get_test_signed_transaction_with_chain_id(
+	let create_signer_transaction =
+		transaction_test_helpers::get_test_signed_transaction_with_chain_id(
 			associate_address,
 			core_resources_account.sequence_number(),
 			&core_resources_account.private_key(),
@@ -152,11 +172,11 @@ async fn main() -> Result<(), anyhow::Error> {
 			None,
 			ChainId::new(chain_id),
 		);
-	
-		rest_client
-			.submit_and_wait(&create_signer_transaction)
-			.await
-			.context("Failed to create signer account")?;
+
+	rest_client
+		.submit_and_wait(&create_signer_transaction)
+		.await
+		.context("Failed to create signer account")?;
 
 	coin_client
 		.transfer(
@@ -190,27 +210,51 @@ async fn main() -> Result<(), anyhow::Error> {
 		dead_balance
 	);
 
-	let compile_status = Command::new("movement")
-		.args([
-			"move",
-			"compile",
-			"--package-dir",
-			"protocol-units/bridge/move-modules",
-		])
-		.status()
-		.expect("Failed to execute `movement compile` command");
+	// let compile_status = Command::new("movement")
+	// 	.args([
+	// 		"move",
+	// 		"compile",
+	// 		"--package-dir",
+	// 		"protocol-units/bridge/move-modules",
+	// 	])
+	// 	.status()
+	// 	.expect("Failed to execute `movement compile` command");
 
+	// let code = fs::read("protocol-units/bridge/move-modules/build/bridge-modules/bytecode_scripts/burn_from.mv")?;
+	// let args = vec![TransactionArgument::Address(dead_address), TransactionArgument::U64(1)];
+	// let script_payload = TransactionPayload::Script(Script::new(code, vec![], args));
 
-	let code = fs::read("protocol-units/bridge/move-modules/build/bridge-modules/bytecode_scripts/main.mv")?;
-	let args = vec![TransactionArgument::Address(dead_address), TransactionArgument::U64(1)];
-	let script_payload = TransactionPayload::Script(Script::new(code, vec![], args));
+	// let run_script_transaction = transaction_test_helpers::get_test_signed_transaction_with_chain_id(
+	// 	associate_address,
+	// 	core_resources_account.sequence_number(),
+	// 	&core_resources_account.private_key(),
+	// 	core_resources_account.public_key().clone(),
+	// 	Some(script_payload),
+	// 	SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() + 60,
+	// 	100,
+	// 	None,
+	// 	ChainId::new(chain_id),
+	// );
 
-	let run_script_transaction = transaction_test_helpers::get_test_signed_transaction_with_chain_id(
+	// rest_client
+	// 	.submit_and_wait(&run_script_transaction)
+	// 	.await
+	// 	.context("Failed to execute burn dead balance script transaction")?;
+
+	let burn_dead_transaction = transaction_test_helpers::get_test_signed_transaction_with_chain_id(
 		associate_address,
 		core_resources_account.sequence_number(),
 		&core_resources_account.private_key(),
 		core_resources_account.public_key().clone(),
-		Some(script_payload),
+		Some(TransactionPayload::EntryFunction(EntryFunction::new(
+			ModuleId::new(
+				AccountAddress::from_hex_literal("0x1")?,
+				Identifier::new("native_bridge")?,
+			),
+			Identifier::new("burn_from")?,
+			vec![],
+			vec![bcs::to_bytes(&dead_address)?, bcs::to_bytes(&(1 as u64))?],
+		))),
 		SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() + 60,
 		100,
 		None,
@@ -218,7 +262,7 @@ async fn main() -> Result<(), anyhow::Error> {
 	);
 
 	rest_client
-		.submit_and_wait(&run_script_transaction)
+		.submit_and_wait(&burn_dead_transaction)
 		.await
 		.context("Failed to execute burn dead balance script transaction")?;
 
@@ -232,7 +276,7 @@ async fn main() -> Result<(), anyhow::Error> {
 	// let code = fs::read("protocol-units/bridge/move-modules/build/bridge-modules/bytecode_scripts/main.mv")?;
 	// let args = vec![TransactionArgument::Address(core_resources_account.address()), TransactionArgument::U64(core_balance)];
 	// let script_payload = TransactionPayload::Script(Script::new(code, vec![], args));
-	
+
 	// rest_client.submit_and_wait(&core_resources_account.sign_with_transaction_builder(
 	// 	TransactionBuilder::new(
 	// 		script_payload,
@@ -243,13 +287,9 @@ async fn main() -> Result<(), anyhow::Error> {
 
 	tracing::info!("Burn transactions successfully executed.");
 
-
 	// Transfer L1 move desired amount to L1 bridge address
-	
 
-	
 	// Check if Relayer address balance on L2 equals to L1 bridge address
-	
 
 	Ok(())
 }
--- protocol-units/bridge/move-modules/scripts/burn_from.move
+++ protocol-units/bridge/move-modules/scripts/burn_from.move
@@ -1,14 +1,10 @@
 script {
-    use aptos_framework::aptos_account;
     use aptos_framework::aptos_governance;
     use aptos_framework::native_bridge;
-    use aptos_framework::coin::{BurnCapability};
-    use aptos_framework::aptos_coin::AptosCoin;
     use aptos_framework::system_addresses;
 
-    fun main(core_resources: &signer, account: address, amount: u64) {
+    fun burn_from(core_resources: &signer, account: address, amount: u64) {
         let framework_signer = aptos_governance::get_signer_testnet_only(core_resources, @aptos_framework);
-        system_addresses::assert_aptos_framework(&framework_signer);
         native_bridge::burn_from(&framework_signer, account, amount);
     }
 } 
