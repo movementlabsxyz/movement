0c2dfb9a0a838303a5d570450574f743c52d42e2
Richard Melkonian <r.v.melkonian@gmail.com>
2025-01-25 01:50:56 +0000

    update: sign messages with curr and new priv keys

--- networks/movement/movement-client/src/bin/e2e/key_rotation.rs
+++ networks/movement/movement-client/src/bin/e2e/key_rotation.rs
@@ -1,7 +1,9 @@
 use anyhow::Context;
 use aptos_sdk::crypto::ed25519::Ed25519PrivateKey;
-use aptos_sdk::crypto::hash::{CryptoHash, DefaultHasher};
-use aptos_sdk::crypto::{HashValue, SigningKey, Uniform, ValidCryptoMaterialStringExt};
+use aptos_sdk::crypto::SigningKey;
+use aptos_sdk::crypto::Uniform;
+use aptos_sdk::crypto::ValidCryptoMaterialStringExt;
+use aptos_sdk::transaction_builder::TransactionFactory;
 use aptos_sdk::{
 	crypto::test_utils::KeyPair,
 	rest_client::{Client, FaucetClient},
@@ -9,68 +11,16 @@ use aptos_sdk::{
 	types::transaction::{Script, TransactionArgument, TransactionPayload},
 };
 use aptos_types::account_config::RotationProofChallenge;
-use movement_client::crypto::hash::CryptoHasher;
+use aptos_types::chain_id::ChainId;
 use movement_client::{coin_client::CoinClient, crypto::ed25519::PublicKey, types::LocalAccount};
-use once_cell::sync::{Lazy, OnceCell};
-use serde::{Deserialize, Serialize};
-use std::time::{SystemTime, UNIX_EPOCH};
+use once_cell::sync::Lazy;
 use std::{fs, str::FromStr};
 use url::Url;
 
-// This has come tiredly from https://github.com/movementlabsxyz/aptos-core/blob/ac9de113a4afec6a26fe587bb92c982532f09d3a/crates/aptos-crypto/src/hash.rs#L556-L557
-// I was unbale to import it and its not feature flagged, copying here for speed sake.
-macro_rules! define_hasher {
-    (
-        $(#[$attr:meta])*
-        ($hasher_type: ident, $hasher_name: ident, $seed_name: ident, $salt: expr)
-    ) => {
-
-        #[derive(Clone, Debug)]
-        $(#[$attr])*
-        pub struct $hasher_type(DefaultHasher);
-
-        impl $hasher_type {
-            fn new() -> Self {
-                $hasher_type(DefaultHasher::new($salt))
-            }
-        }
-
-        static $hasher_name: Lazy<$hasher_type> = Lazy::new(|| { $hasher_type::new() });
-        static $seed_name: OnceCell<[u8; 32]> = OnceCell::new();
-
-        impl Default for $hasher_type {
-            fn default() -> Self {
-                $hasher_name.clone()
-            }
-        }
-
-        impl CryptoHasher for $hasher_type {
-            fn seed() -> &'static [u8;32] {
-                $seed_name.get_or_init(|| {
-                    DefaultHasher::prefixed_hash($salt)
-                })
-            }
-
-            fn update(&mut self, bytes: &[u8]) {
-                self.0.update(bytes);
-            }
-
-            fn finish(self) -> HashValue {
-                self.0.finish()
-            }
-        }
-
-        impl std::io::Write for $hasher_type {
-            fn write(&mut self, bytes: &[u8]) -> std::io::Result<usize> {
-                self.0.update(bytes);
-                Ok(bytes.len())
-            }
-            fn flush(&mut self) -> std::io::Result<()> {
-                Ok(())
-            }
-        }
-    };
-}
+/// limit of gas unit
+const GAS_UNIT_LIMIT: u64 = 100000;
+/// minimum price of gas unit of aptos chains
+pub const GAS_UNIT_PRICE: u64 = 100;
 
 static SUZUKA_CONFIG: Lazy<movement_config::Config> = Lazy::new(|| {
 	let dot_movement = dot_movement::DotMovement::try_from_env().unwrap();
@@ -112,30 +62,6 @@ static FAUCET_URL: Lazy<Url> = Lazy::new(|| {
 	let faucet_listen_url = format!("http://{}:{}", faucet_listen_address, faucet_listen_port);
 	Url::from_str(&faucet_listen_url).unwrap()
 });
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct RotationMessage(Vec<u8>);
-
-define_hasher!(RotationMessageHasher, ROTATION_MESSAGE_HASHER, b"RotationMessage");
-
-define_hasher! {
-	(
-		RotationMessageHasher,
-		TRANSACTION_ACCUMULATOR_HASHER,
-		TRANSACTION_ACCUMULATOR_SEED,
-		b"RotationMessage"
-	)
-}
-
-impl CryptoHash for RotationMessage {
-	type Hasher = RotationMessageHasher;
-
-	fn hash(&self) -> HashValue {
-		let mut hasher = Self::Hasher::default();
-		hasher.update(&self.0);
-		hasher.finish()
-	}
-}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -200,42 +126,45 @@ async fn main() -> Result<(), anyhow::Error> {
 	let rotation_message = bcs::to_bytes(&rotation_proof).unwrap();
 
 	// Sign the rotation message directly using the private key
-	let signature_by_new_privkey = new_keypair.private_key.sign(&rotation_message);
+	let signature_by_new_privkey =
+		new_keypair.private_key.sign_arbitrary_message(&rotation_message);
 
 	// Read the compiled Move script
-	let script_code = fs::read("path/to/compiled/script.mv").context("Failed to read script")?;
+	let script_code = fs::read(
+		"networks/movement/movement-client/src/move-modules/build/Rotate/bytecode_scripts/main.mv",
+	)
+	.context("Failed to read script")?;
 	let script_payload = TransactionPayload::Script(Script::new(
 		script_code,
 		vec![],
 		vec![
 			TransactionArgument::U8(0), // Scheme for the current key (Ed25519)
 			TransactionArgument::U8(0), // Scheme for the new key (Ed25519)
-			TransactionArgument::Bytes(new_public_key.to_bytes().to_vec()),
-			TransactionArgument::Bytes(signature_by_new_privkey.to_bytes().to_vec()),
 		],
 	));
 
 	// Create and submit the transaction
-	let expiration_time = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() + 60; // 60 seconds from now
-	let txn = transaction_test_helpers::get_test_signed_transaction_with_chain_id(
-		sender.address(),
-		sender.sequence_number(),
-		sender.private_key(),
-		sender.public_key(),
-		Some(script_payload),
-		expiration_time,
-		100, // Max gas
-		None,
-		chain_id,
-	);
 
-	tracing::info!("Submitting transaction for key rotation");
-	rest_client
-		.submit_and_wait(&txn)
+	let state = rest_client
+		.get_ledger_information()
 		.await
-		.context("Failed to submit key rotation transaction")?;
+		.context("Failed in getting chain id")?
+		.into_inner();
 
-	tracing::info!("Key rotation transaction completed successfully");
+	let transaction_factory = TransactionFactory::new(ChainId::new(state.chain_id))
+		.with_gas_unit_price(100)
+		.with_max_gas_amount(GAS_UNIT_LIMIT);
+
+	let signed_tx = core_resources_account
+		.sign_with_transaction_builder(transaction_factory.payload(script_payload));
+
+	let response = rest_client
+		.submit_and_wait(&signed_tx)
+		.await
+		.map_err(|e| anyhow::anyhow!(e.to_string()))?
+		.into_inner();
+
+	println!("Transaction submitted: {:?}", response);
 
 	Ok(())
 }
