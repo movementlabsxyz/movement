87c68d698ff7b45805b44380321743bb992cd6ea
Liam Monninger <l.mak.monninger@gmail.com>
2025-01-26 12:31:04 -0800

    fix: add governed gas pool ops.

--- networks/movement/movement-full-node/src/admin/mod.rs
+++ networks/movement/movement-full-node/src/admin/mod.rs
@@ -1,5 +1,6 @@
 pub mod bring_up;
 pub mod framework;
+pub mod governed_gas_pool;
 pub mod mcr;
 pub mod ops;
 pub mod rotate_key;
@@ -12,6 +13,14 @@ pub enum Admin {
 	Mcr(mcr::Mcr),
 	#[clap(subcommand)]
 	RotateKey(rotate_key::RotateKey),
+	#[clap(subcommand)]
+	BringUp(bring_up::BringUp),
+	#[clap(subcommand)]
+	GovernedGasPool(governed_gas_pool::GovernedGasPool),
+	#[clap(subcommand)]
+	Ops(ops::Ops),
+	#[clap(subcommand)]
+	Framework(framework::Framework),
 }
 
 impl Admin {
@@ -19,6 +28,10 @@ impl Admin {
 		match self {
 			Admin::Mcr(mcr) => mcr.execute().await,
 			Admin::RotateKey(rotate_key) => rotate_key.execute().await,
+			Admin::BringUp(bring_up) => bring_up.execute().await,
+			Admin::GovernedGasPool(governed_gas_pool) => governed_gas_pool.execute().await,
+			Admin::Ops(ops) => ops.execute().await,
+			Admin::Framework(framework) => framework.execute().await,
 		}
 	}
 }
