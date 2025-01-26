8e3b690541f82f10af1e98321db6ddfd17b5d47d
Liam Monninger <l.mak.monninger@gmail.com>
2025-01-26 11:59:21 -0800

    fix: add mint lock.

--- networks/movement/movement-full-node/src/admin/ops/mod.rs
+++ networks/movement/movement-full-node/src/admin/ops/mod.rs
@@ -1,11 +1,17 @@
+pub mod mint_lock;
+
 use clap::Subcommand;
 
 #[derive(Subcommand, Debug)]
 #[clap(rename_all = "kebab-case", about = "Commands for bespoke network operations")]
-pub enum Ops {}
+pub enum Ops {
+	MintLock(mint_lock::MintLock),
+}
 
 impl Ops {
 	pub async fn execute(&self) -> Result<(), anyhow::Error> {
-		match self {}
+		match self {
+			Ops::MintLock(mint_lock) => mint_lock.execute().await,
+		}
 	}
 }
