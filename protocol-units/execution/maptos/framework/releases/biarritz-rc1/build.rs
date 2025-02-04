efa883c3318254147e7881acfd037b957374093d
Liam Monninger <l.mak.monninger@gmail.com>
2025-02-03 22:59:29 -0800

    fix: stabilize from upstream.

--- protocol-units/execution/maptos/framework/releases/biarritz-rc1/build.rs
+++ protocol-units/execution/maptos/framework/releases/biarritz-rc1/build.rs
@@ -4,7 +4,7 @@ use maptos_framework_release_util::commit_hash_with_script;
 commit_hash_with_script!(
 	BiarritzRc1,                                         // Struct name
 	"https://github.com/movementlabsxyz/aptos-core.git", // Repository URL
-	"8b2f9f8fa7718118be451e92fb005ca2dee6a006",          // Commit hash
+	"d86339c130c3a9afd9413db95412b7586b78c905",          // Commit hash
 	6,                                                   // Bytecode version
 	"biarritz-rc1.mrb",                                  // MRB file name
 	"CACHE_BIARRITZ_RC1_FRAMEWORK_RELEASE"               // Cache environment variable for Elsa
