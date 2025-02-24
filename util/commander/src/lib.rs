0dfc44b5f23a941724251ebc2f117fef343c314b
Mikhail Zabaluev <mikhail.zabaluev@movementlabs.xyz>
2025-02-24 12:15:44 +0200

    chore(commander): remove spawn_command
    Remove the dead code.

--- util/commander/src/lib.rs
+++ util/commander/src/lib.rs
@@ -4,7 +4,6 @@ use itertools::Itertools;
 use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::process::Command;
 use tokio::signal::unix::{signal, SignalKind};
-use tokio::task::JoinHandle;
 use tracing::info;
 
 use std::ffi::OsStr;
@@ -115,58 +114,6 @@ where
 	}
 
 	Ok(stdout_output)
-}
-
-/// Runs a command, piping its output to stdout and stderr, and returns the stdout output if successful.
-pub async fn spawn_command(
-	command: String,
-	args: Vec<String>,
-) -> Result<(Option<u32>, JoinHandle<Result<String, anyhow::Error>>)> {
-	// print command out with args joined by space
-	tracing::info!("spawn command: {} {}", command, args.join(" "));
-
-	let mut child = Command::new(&command)
-		.args(&args)
-		.stdout(Stdio::piped())
-		.stderr(Stdio::piped())
-		.spawn()?;
-
-	let process_id = child.id();
-	let join_handle = tokio::spawn({
-		async move {
-			let stdout = child.stdout.take().ok_or_else(|| {
-				anyhow::anyhow!("Failed to capture standard output from command {}", command)
-			})?;
-			let stderr = child.stderr.take().ok_or_else(|| {
-				anyhow::anyhow!("Failed to capture standard error from command {}", command)
-			})?;
-
-			let mut stdout_output = String::new();
-			let mut stderr_output = String::new();
-
-			let stdout_writer = io::stdout();
-			let stderr_writer = io::stderr();
-
-			let stdout_future = pipe_output(stdout, stdout_writer, &mut stdout_output);
-			let stderr_future = pipe_error_output(stderr, stderr_writer, &mut stderr_output);
-
-			let _ = try_join(stdout_future, stderr_future).await;
-
-			let status = child.wait().await?;
-			if !status.success() {
-				return Err(anyhow::anyhow!(
-					"Command {} spawn failed with args {:?}\nError Output: {}",
-					command,
-					args,
-					stderr_output
-				));
-			}
-
-			Ok(stdout_output)
-		}
-	});
-
-	Ok((process_id, join_handle))
 }
 
 #[cfg(test)]
