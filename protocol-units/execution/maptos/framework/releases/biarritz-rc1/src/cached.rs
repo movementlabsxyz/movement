a8c174f7b96196c34ac2a85355a75393f12db632
Liam Monninger <l.mak.monninger@gmail.com>
2025-02-26 10:12:39 -0800

    fix: transaction pipe.

--- protocol-units/execution/maptos/framework/releases/biarritz-rc1/src/cached.rs
+++ protocol-units/execution/maptos/framework/releases/biarritz-rc1/src/cached.rs
@@ -11,7 +11,8 @@ generate_gas_upgrade_module!(gas_upgrade, BiarritzRc1, {
 	let mut gas_parameters = AptosGasParameters::initial();
 	gas_parameters.vm.txn.max_transaction_size_in_bytes = GasQuantity::new(100_000_000);
 	gas_parameters.vm.txn.max_execution_gas = GasQuantity::new(10_000_000_000);
-
+	gas_parameters.vm.txn.min_price_per_gas_unit = GasQuantity::new(20_000_000);
+	gas_parameters.vm.txn.min_price_per_gas_unit = GasQuantity::new(20_000_000);
 	aptos_types::on_chain_config::GasScheduleV2 {
 		feature_version: aptos_gas_schedule::LATEST_GAS_FEATURE_VERSION,
 		entries: gas_parameters
