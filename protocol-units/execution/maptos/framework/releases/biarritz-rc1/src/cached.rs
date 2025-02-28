b8af60b880c123a9725fff6fcd5ce488bcf864e8
Liam Monninger <l.mak.monninger@gmail.com>
2025-02-28 11:20:45 -0800

    fix: gas fee.

--- protocol-units/execution/maptos/framework/releases/biarritz-rc1/src/cached.rs
+++ protocol-units/execution/maptos/framework/releases/biarritz-rc1/src/cached.rs
@@ -4,7 +4,7 @@ use maptos_framework_release_util::mrb_release;
 mrb_release!(
 	BiarritzRc1,
 	BIARRTIZ_RC1,
-	"d86339c130c3a9afd9413db95412b7586b78c905-biarritz-rc1.mrb"
+	"4bc8e26943942286f2f010b041b6527336dbf591-biarritz-rc1.mrb"
 );
 
 generate_gas_upgrade_module!(gas_upgrade, BiarritzRc1, {
--- protocol-units/execution/maptos/framework/releases/biarritz-rc1/src/lib.rs
+++ protocol-units/execution/maptos/framework/releases/biarritz-rc1/src/lib.rs
@@ -7,7 +7,7 @@ use maptos_framework_release_util::commit_hash_with_script;
 commit_hash_with_script!(
 	BiarritzRc1,                                         // Struct name
 	"https://github.com/movementlabsxyz/aptos-core.git", // Repository URL
-	"d86339c130c3a9afd9413db95412b7586b78c905",          // Commit hash
+	"4bc8e26943942286f2f010b041b6527336dbf591",          // Commit hash
 	6,                                                   // Bytecode version
 	"biarritz-rc1.mrb",                                  // MRB file name
 	"CACHE_BIARRITZ_RC1_FRAMEWORK_RELEASE"               // Cache environment variable for Elsa
