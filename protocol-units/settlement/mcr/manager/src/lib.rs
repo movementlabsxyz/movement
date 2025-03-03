13b2c24fbf698ff80401541291750edaae506a98
apenzk <dr.andreas.penzkofer@gmail.com>
2025-03-03 14:47:14 +0100

    fix cargo check

--- protocol-units/settlement/mcr/manager/src/lib.rs
+++ protocol-units/settlement/mcr/manager/src/lib.rs
@@ -1,4 +1,4 @@
-use movement_types::block::{BlockCommitment, BlockCommitmentEvent};
+use movement_types::block::{SuperBlockCommitment, SuperBlockCommitmentEvent};
 use tokio_stream::Stream;
 
 mod manager;
@@ -6,13 +6,13 @@ mod manager;
 pub use manager::Manager as McrSettlementManager;
 
 pub type CommitmentEventStream =
-	std::pin::Pin<Box<dyn Stream<Item = Result<BlockCommitmentEvent, anyhow::Error>> + Send>>;
+	std::pin::Pin<Box<dyn Stream<Item = Result<SuperBlockCommitmentEvent, anyhow::Error>> + Send>>;
 
 #[async_trait::async_trait]
 pub trait McrSettlementManagerOperations {
 	/// Adds a block commitment to the manager queue.
 	async fn post_block_commitment(
 		&self,
-		block_commitment: BlockCommitment,
+		block_commitment: SuperBlockCommitment,
 	) -> Result<(), anyhow::Error>;
 }
