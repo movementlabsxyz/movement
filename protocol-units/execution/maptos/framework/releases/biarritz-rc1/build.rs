18997fc9e5e3b302434f7cdd90c14df4571bb82a
Richard Melkonian <35300528+0xmovses@users.noreply.github.com>
2025-03-05 18:25:31 -0800

    update: aptos-core

--- protocol-units/execution/maptos/framework/releases/biarritz-rc1/build.rs
+++ protocol-units/execution/maptos/framework/releases/biarritz-rc1/build.rs
@@ -5,7 +5,7 @@ use maptos_framework_release_util::commit_hash_with_script;
 commit_hash_with_script!(
 	BiarritzRc1,                                         // Struct name
 	"https://github.com/movementlabsxyz/aptos-core.git", // Repository URL
-	"d8306f3a42e26dafd9eb1139682645fe3f9e490b",          // Commit hash
+	"ec6191fe921bca19d86b7cb77da34eb5c3e79c09",          // Commit hash
 	6,                                                   // Bytecode version
 	"biarritz-rc1.mrb",                                  // MRB file name
 	"CACHE_BIARRITZ_RC1_FRAMEWORK_RELEASE"               // Cache environment variable for Elsa
