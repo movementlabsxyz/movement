fbea1074fe797d461ca9c1b2db19d98888e3bf18
musitdev <philippe.delrieu@free.fr>
2025-03-24 11:12:16 +0100

    feat: add submit_batch cmd to full node exe

--- networks/movement/movement-full-node/src/da/mod.rs
+++ networks/movement/movement-full-node/src/da/mod.rs
@@ -1,4 +1,5 @@
-pub mod stream_blocks;
+mod stream_blocks;
+mod submit_batch;
 
 use clap::Subcommand;
 
@@ -6,12 +7,14 @@ use clap::Subcommand;
 #[clap(rename_all = "kebab-case", about = "Commands for intereacting with the DA")]
 pub enum Da {
 	StreamBlocks(stream_blocks::StreamBlocks),
+	SubmitBatch(submit_batch::SubmitBatch),
 }
 
 impl Da {
 	pub async fn execute(&self) -> Result<(), anyhow::Error> {
 		match self {
 			Da::StreamBlocks(stream_blocks) => stream_blocks.execute().await,
+			Da::SubmitBatch(submit_batch) => submit_batch.execute().await,
 		}
 	}
 }
