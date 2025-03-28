00761506829b0134800db2f428fa9a7d7a971aa6
Philippe Delrieu <philippe.delrieu@free.fr>
2025-03-28 10:08:45 +0100

    feat: Implement grpc server write_batch (#1155)
    Co-authored-by: Richard Melkonian <35300528+0xmovses@users.noreply.github.com>

--- protocol-units/da-sequencer/node/src/batch.rs
+++ protocol-units/da-sequencer/node/src/batch.rs
@@ -1,24 +1,62 @@
 use crate::error::DaSequencerError;
 use aptos_crypto::ed25519::{Ed25519PublicKey, Ed25519Signature};
+use core::convert::TryFrom;
 
 #[derive(Debug)]
 pub struct RawData {
-	pub bytes: Vec<u8>,
+	pub data: Vec<u8>,
 }
 
 #[derive(Debug)]
 pub struct FullnodeTx {}
 
 #[derive(Debug)]
-pub struct DaBatch<Data> {
-	data: Data,
-	signature: Ed25519Signature,
-	signer: Ed25519PublicKey,
+pub struct DaBatch<D> {
+	pub data: D,
+	pub signature: Ed25519Signature,
+	pub signer: Ed25519PublicKey,
+	pub timestamp: u64,
+}
+
+impl DaBatch<RawData> {
+	pub fn now(signer: Ed25519PublicKey, signature: Ed25519Signature, data: Vec<u8>) -> Self {
+		let timestamp = chrono::Utc::now().timestamp_micros() as u64;
+		DaBatch { data: RawData { data }, signature, signer, timestamp }
+	}
 }
 
 /// Batch write blobs.
-fn validate_batch(
+pub fn validate_batch(
 	new_batch: DaBatch<RawData>,
 ) -> std::result::Result<DaBatch<FullnodeTx>, DaSequencerError> {
 	todo!()
+}
+
+pub fn serialize_full_node_batch(
+	signer: Ed25519PublicKey,
+	signature: Ed25519Signature,
+	mut data: Vec<u8>,
+) -> Vec<u8> {
+	let mut serialized: Vec<u8> = Vec::with_capacity(64 + 32 + data.len());
+	serialized.extend_from_slice(&signer.to_bytes());
+	serialized.extend_from_slice(&signature.to_bytes());
+	serialized.append(&mut data);
+	serialized
+}
+
+pub fn deserialize_full_node_batch(
+	data: Vec<u8>,
+) -> std::result::Result<(Ed25519PublicKey, Ed25519Signature, Vec<u8>), DaSequencerError> {
+	let (pubkey_deserialized, rest) = data.split_at(32);
+	let (sign_deserialized, vec_deserialized) = rest.split_at(64);
+
+	// Convert the slices back into arrays
+	let pub_key_bytes: [u8; 32] = pubkey_deserialized.try_into()?;
+	let signature_bytes: [u8; 64] = sign_deserialized.try_into()?;
+
+	let public_key = Ed25519PublicKey::try_from(pub_key_bytes.as_slice())?;
+	let signature = Ed25519Signature::try_from(signature_bytes.as_slice())?;
+
+	let data: Vec<u8> = vec_deserialized.to_vec();
+	Ok((public_key, signature, data))
 }
--- protocol-units/da-sequencer/node/src/lib.rs
+++ protocol-units/da-sequencer/node/src/lib.rs
@@ -1,15 +1,39 @@
 use crate::error::DaSequencerError;
+use crate::server::GrpcRequests;
 use movement_da_sequencer_config::DaSequencerConfig;
+use tokio::sync::mpsc;
 
-mod batch;
+pub mod batch;
 mod block;
 mod celestia;
-mod error;
+pub mod error;
 mod server;
 mod storage;
 
 /// Run Da sequencing loop.
 /// This function only return in case of error that indicate a crash of the node.
-pub fn run(config: DaSequencerConfig) -> std::result::Result<(), DaSequencerError> {
-	todo!()
+pub async fn run(
+	config: DaSequencerConfig,
+	mut request_rx: mpsc::Receiver<GrpcRequests>,
+) -> std::result::Result<(), DaSequencerError> {
+	let mut produce_block_interval = tokio::time::interval(tokio::time::Duration::from_millis(500)); //todo put interval value in the config.
+	loop {
+		tokio::select! {
+			Some(grpc_request) = request_rx.recv() => {
+				match grpc_request {
+					GrpcRequests::StartBlockStream { callback } => todo!(),
+					GrpcRequests::GetBlockHeight { block_height, callback } => todo!(),
+					GrpcRequests::WriteBatch(batch) => {
+						//send batch to the storage.
+					},
+
+				}
+			}
+			_ = produce_block_interval.tick() => {
+				//produce one block
+
+				//propagate the new block.
+			}
+		}
+	}
 }
