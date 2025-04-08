0f0cbe6415b869d58eb8c21015067e520c0f3929
Richard Melkonian <r.v.melkonian@gmail.com>
2025-04-08 10:00:29 +0100

    chore: remove old mock file

--- protocol-units/da-sequencer/node/src/tests/mock.rs
+++ protocol-units/da-sequencer/node/src/tests/mock.rs
@@ -149,36 +149,3 @@ impl DaSequencerStorage for StorageMock {
 		self.inner.lock().unwrap().current_height.into()
 	}
 }
-
-#[derive(Debug, Clone)]
-pub struct CelestiaMock {}
-
-impl CelestiaMock {
-	pub fn new() -> Self {
-		CelestiaMock {}
-	}
-}
-
-impl DaSequencerExternalDa for CelestiaMock {
-	fn send_block(
-		&self,
-		block: SequencerBlockDigest,
-	) -> impl Future<Output = Result<(), DaSequencerError>> + Send {
-		futures::future::ready(Ok(()))
-	}
-
-	fn get_blobs_at_height(
-		&self,
-		height: CelestiaHeight,
-	) -> impl Future<Output = Result<Option<Vec<Blob>>, DaSequencerError>> + Send {
-		//TODO return dummy error for now.
-		futures::future::ready(Err(DaSequencerError::DeserializationFailure))
-	}
-
-	fn bootstrap(
-		&self,
-		current_block_height: BlockHeight,
-	) -> impl Future<Output = Result<(), DaSequencerError>> + Send {
-		futures::future::ready(Ok(()))
-	}
-}
