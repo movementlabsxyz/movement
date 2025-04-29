ef7d6d5cdda72bbc4a858b033d3b26495a685340
Richard Melkonian <r.v.melkonian@gmail.com>
2025-04-29 14:36:43 +0100

    chore: update build with correct aptos hash

--- protocol-units/execution/maptos/framework/releases/post-l1-merge/build.rs
+++ protocol-units/execution/maptos/framework/releases/post-l1-merge/build.rs
@@ -3,8 +3,8 @@ use maptos_framework_release_util::commit_hash_with_script;
 commit_hash_with_script!(
 	PostL1Merge,                                         // Struct name
 	"https://github.com/movementlabsxyz/aptos-core.git", // Repository URL
-	"d00f5e5ef3179919b3fc8245ac774f8509ed6a3e",          // Commit hash
+	"035700578e23aff9bff4aba0a415cf26cf7731a5",          // Commit hash
 	6,                                                   // Bytecode version
 	"biarritz-rc1.mrb",                                  // MRB file name
-	"CACHE_POST_L1_MERGE_FRAMEWORK_RELEASE"              // Cache environment variable for Elsa
+	"POST_L1_MERGE_FRAMEWORK_RELEASE"                    // Cache environment variable for Elsa
 );
