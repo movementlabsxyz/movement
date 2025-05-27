cc79baa68c5c863fbaa7b1b3109542408a139229
Andy Golay <andygolay@gmail.com>
2025-05-27 11:24:27 -0400

    fix: build errors

--- protocol-units/settlement/mcr/setup/src/deploy/mod.rs
+++ protocol-units/settlement/mcr/setup/src/deploy/mod.rs
@@ -1,22 +1,5 @@
-use alloy::providers::Provider;
-use alloy::providers::ProviderBuilder;
-use alloy::signers::Signer;
-use alloy_network::EthereumWallet;
-use alloy_network::TransactionBuilder;
-use alloy_primitives::U256;
-use anyhow::anyhow;
-use anyhow::Context;
-use commander::run_command;
 use dot_movement::DotMovement;
-use mcr_settlement_client::eth_client::MCR;
 use mcr_settlement_config::{common, Config};
-use movement_signer::cryptography::secp256k1::Secp256k1;
-use movement_signer_aws_kms::hsm::AwsKms;
-use movement_signer_loader::identifiers::SignerIdentifier;
-use movement_signing_eth::HsmSigner;
-use serde_json::Value;
-use std::str::FromStr;
-use tracing::info;
 
 /// The local setup strategy for MCR settlement
 #[derive(Debug, Clone)]
