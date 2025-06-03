2ac33c0e1b443057eeeb80503ff8c8d1c100d17d
musitdev <philippe.delrieu@free.fr>
2025-06-03 10:00:02 +0200

    use Restic to do the backup/restore

--- docs/movement-node/run-fullnode/scripts/devnet/backup.sh
+++ docs/movement-node/run-fullnode/scripts/devnet/backup.sh
@@ -3,7 +3,7 @@
 export DOT_MOVEMENT_PATH=$HOME/.movement
 export AWS_REGION="us-west-2"
 export RESTIC_PASSWORD="movebackup"
-export RESTIC_HOST="mainnet_fullnode"
-export SYNC_BUCKET="movement-sync-mainnet"
+export RESTIC_HOST="devnet_fullnode"
+export SYNC_BUCKET="movement-sync-devnet"
 
 /usr/bin/docker compose -f ./movement/docker/compose/movement-full-node/snapshot/docker-compose.backup.yml up --force-recreate
