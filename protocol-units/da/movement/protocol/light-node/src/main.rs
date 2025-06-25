06b1562d151c846f932f39f69bc39a102387b9ad
Icarus <46900041+0xIcarus@users.noreply.github.com>
2025-06-25 21:09:07 +0530

    feat: update tracing crate to thin aptos-telemetry wrapper (#1246)

--- protocol-units/da/movement/protocol/light-node/src/main.rs
+++ protocol-units/da/movement/protocol/light-node/src/main.rs
@@ -7,9 +7,6 @@ use movement_signer_loader::LoadedSigner;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let tracing_config = movement_tracing::Config::default();
-	let _guard = movement_tracing::init_tracing_subscriber(tracing_config);
-
 	let dot_movement = dot_movement::DotMovement::try_from_env()?;
 	let config_path = dot_movement.get_config_json_path();
 	let config_file = tokio::fs::File::open(config_path).await?;
