5d3ec6cb63cc0e6e28f5afff0b62440f60fc59ee
Anirudh Prasad <anirudhprasad131@gmail.com>
2025-06-30 16:41:03 +0530

    fix: initialize individual tracing for full node

--- networks/movement/movement-full-node/src/main.rs
+++ networks/movement/movement-full-node/src/main.rs
@@ -1,12 +1,14 @@
 #![forbid(unsafe_code)]
-
 use clap::*;
 use movement_full_node::MovementFullNode;
-
+use tracing_subscriber::EnvFilter;
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-	let suzuka_util = MovementFullNode::parse();
-	let result = suzuka_util.execute().await;
-
-	result
+    // Initialize default tracing
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")))
+        .init();
+    let suzuka_util = MovementFullNode::parse();
+    let result = suzuka_util.execute().await;
+    result
 }
