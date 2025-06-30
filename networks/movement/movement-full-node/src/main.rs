c42fbb85362c692752cc7d6f301b61af7c54ad66
Icarus <46900041+0xIcarus@users.noreply.github.com>
2025-06-30 17:00:28 +0530

    fix: initialize individual tracing for full node (#1267)

--- networks/movement/movement-full-node/src/main.rs
+++ networks/movement/movement-full-node/src/main.rs
@@ -1,12 +1,16 @@
 #![forbid(unsafe_code)]
-
 use clap::*;
 use movement_full_node::MovementFullNode;
-
+use tracing_subscriber::EnvFilter;
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+	// Initialize default tracing
+	tracing_subscriber::fmt()
+		.with_env_filter(
+			EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
+		)
+		.init();
 	let suzuka_util = MovementFullNode::parse();
 	let result = suzuka_util.execute().await;
-
 	result
 }
