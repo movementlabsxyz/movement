8dd4ce259c32862c64d7bf3a4d44065b5a9cefd5
musitdev <philippe.delrieu@free.fr>
2025-08-26 16:05:22 +0200

    test docker script and remove comments

--- docker/scripts/l1-migration-update-epoch-duration.sh
+++ docker/scripts/l1-migration-update-epoch-duration.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -e
 
 export DOT_MOVEMENT_PATH=/.movement
-export CONTAINER_REV=d9f8180-amd64
+export CONTAINER_REV=63d8719-amd64
 export HOME="$(pwd)"
-export MVT_NODE_REST_URL="http://192.168.88.161:30731"
+export MVT_NODE_REST_URL="http://movement-full-node:30731"
 
-DOT_MOVEMENT_PATH="$(pwd)/.movement" cargo run -p movement-full-node -- admin l1-migration change-epoch
-
-#/usr/bin/docker run -v $HOME/.movement:/.movement --rm ghcr.io/movementlabsxyz/movement-full-node:${CONTAINER_REV} admin l1-migration change-epoch
+/usr/bin/docker run -e MVT_NODE_REST_URL=$MVT_NODE_REST_URL -e DOT_MOVEMENT_PATH=$DOT_MOVEMENT_PATH -v $HOME/.movement:/.movement --rm ghcr.io/movementlabsxyz/movement-full-node:${CONTAINER_REV} admin l1-migration change-epoch
 
